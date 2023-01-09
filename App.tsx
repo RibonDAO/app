dd7c955d51159f271d20bf3e26f72fb94aefe66d
Nicholas Marques <nnmarques97@gmail.com>
2023-01-09 12:16:23 -0300

    chore: add eslint plugins

--- App.tsx
+++ App.tsx
@@ -1,18 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './src/hooks/useCachedResources';
-import useColorScheme from './src/hooks/useColorScheme';
-import Navigation from './src/config/navigation';
-import {useEffect} from "react";
-import {registerForPushNotificationsAsync} from "./src/services/notifications";
+import useCachedResources from "./src/hooks/useCachedResources";
+import useColorScheme from "./src/hooks/useColorScheme";
+import Navigation from "./src/config/navigation";
+import { useEffect } from "react";
+import { registerForPushNotificationsAsync } from "./src/services/notifications";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
   useEffect(() => {
-    registerForPushNotificationsAsync().then(token => console.log(token));
+    registerForPushNotificationsAsync().then((token) => console.log(token));
   }, []);
 
   if (!isLoadingComplete) {
