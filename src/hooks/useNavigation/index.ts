c1716f2f210779bf6dfc3a71f39d94ecaec457c5
Nicholas Marques <nnmarques97@gmail.com>
2023-01-10 16:22:16 -0300

    feat: add useNavigation hook

---
+++ src/hooks/useNavigation/index.ts
@@ -0,0 +1,18 @@
+import { useNavigation as useNavigationNative } from "@react-navigation/native";
+
+export function useNavigation() {
+  const { navigate, goBack } = useNavigationNative();
+
+  function navigateTo(screenName: string, params: Record<any, any>) {
+    navigate(screenName as never, params as never);
+  }
+
+  function popNavigation() {
+    goBack();
+  }
+
+  return {
+    navigateTo,
+    popNavigation,
+  };
+}
