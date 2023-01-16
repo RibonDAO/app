e6fdaf5ba7571a98a90d9b1b85f0647a0a88f310
Nicholas Marques <devnicknish@gmail.com>
2023-01-16 10:44:44 -0300

    feat: add initializeApi

--- App.tsx
+++ App.tsx
@@ -1,17 +1,18 @@
 import "./global";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import useCachedResources from "./src/hooks/useCachedResources";
-import useColorScheme from "./src/hooks/useColorScheme";
-import Navigation from "./src/config/navigation";
 import { useEffect } from "react";
 import WalletConnectProvider from "@walletconnect/react-native-dapp";
-import { registerForPushNotificationsAsync } from "./src/services/notifications";
 import { QueryClientComponent } from "@ribon.io/shared/hooks";
 import { useFonts } from "expo-font";
 import "./i18n.config";
 import { Platform } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { registerForPushNotificationsAsync } from "./src/services/notifications";
+import Navigation from "./src/config/navigation";
+import useColorScheme from "./src/hooks/useColorScheme";
+import useCachedResources from "./src/hooks/useCachedResources";
+import { initializeApi } from "./src/services/api";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -25,13 +26,17 @@ export default function App() {
     registerForPushNotificationsAsync().then((token) => console.log(token));
   }, []);
 
+  useEffect(() => {
+    initializeApi();
+  }, []);
+
   if (!isLoadingComplete || !fontsLoaded) {
     return null;
   } else {
     return (
       <WalletConnectProvider
         redirectUrl={
-          Platform.OS === "web" ? window.location.origin : `ribon://`
+          Platform.OS === "web" ? window.location.origin : "ribon://"
         }
         storageOptions={{
           asyncStorage: AsyncStorage as any,
