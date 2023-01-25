01ca85eb0e108a66e4a51708512a9583f682f974
Yan Galli <yangalli@hotmail.com>
2023-01-25 14:48:12 -0300

    feat: add configs for sentry

--- src/hooks/useCachedResources.ts
+++ src/hooks/useCachedResources.ts
@@ -6,12 +6,21 @@ import { initializeApi } from "services/api";
 import SpaceMono from "../assets/fonts/SpaceMono-Regular.ttf";
 import GambarinoRegular from "../assets/fonts/Gambarino-Regular.ttf";
 import Inter from "../assets/fonts/Inter.ttf";
+import * as Sentry from 'sentry-expo';
 
 export default function useCachedResources() {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
 
   useEffect(() => {
     initializeApi();
+  }, []);
+
+  useEffect(() => {
+    Sentry.init({
+      dsn: "https://c39226956ee34786a8acdb34123f7d3b@o409844.ingest.sentry.io/4504565365473280",
+      enableInExpoDevelopment: false,
+      debug: true
+    });
   }, []);
 
   useEffect(() => {
