0ef4ed61c449eef1f9ad4b9b0a71624f634c5d7c
Nicholas Marques <devnicknish@gmail.com>
2023-01-26 13:14:10 -0300

    refactor: add Icon component default export

--- src/components/atomics/Icon/index.tsx
+++ src/components/atomics/Icon/index.tsx
@@ -2,6 +2,7 @@ import { createIconSet } from "@expo/vector-icons";
 import MaterialIconsRoundedGlyphMap from "assets/fonts/MaterialSymbolsRounded.json";
 import MaterialIconsOutlineGlyphMap from "assets/fonts/MaterialSymbolsOutline.json";
 import MaterialIconsSharpGlyphMap from "assets/fonts/MaterialSymbolsSharp.json";
+import { IconProps } from "@expo/vector-icons/build/createIconSet";
 
 const IconRounded = createIconSet(
   MaterialIconsRoundedGlyphMap,
@@ -21,5 +22,23 @@ const IconSharp = createIconSet(
   "material-symbols-sharp.ttf",
 );
 
+interface Props
+  extends IconProps<any> {
+  type: "sharp" | "rounded" | "outlined";
+}
+function Icon(props: Props) {
+  const { type, ...rest } = props;
+  switch (type) {
+    case "sharp":
+      return <IconSharp {...rest} />;
+    case "rounded":
+      return <IconRounded {...rest} />;
+    case "outlined":
+      return <IconOutline {...rest} />;
+    default:
+      return <IconRounded {...rest} />;
+  }
+}
+
 export { IconRounded, IconOutline, IconSharp };
-export default IconRounded;
+export default Icon;
