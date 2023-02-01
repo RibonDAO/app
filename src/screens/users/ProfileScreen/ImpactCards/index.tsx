47eecb0578c79e70ebf14119f4f758f96eca0dcc
Yan Galli <yangalli@hotmail.com>
2023-02-01 16:41:01 -0300

    feat: updating impact card with user data

--- src/screens/users/ProfileScreen/ImpactCards/index.tsx
+++ src/screens/users/ProfileScreen/ImpactCards/index.tsx
@@ -1,18 +1,25 @@
 import { View } from "components/Themed";
 import S from "./styles";
-import { useState } from "react";
+import { useCallback } from "react";
 import ImpactCard from "../ImpactCard";
+import { useCurrentUser } from "contexts/currentUserContext";
+import { useStatistics } from "@ribon.io/shared/hooks";
 
 function ImpactCards(): JSX.Element {
-  const [impacts] = useState([
-    { name: "Donated tickets", impact: 200, iconName: "confirmation_number" },
-    { name: "Donated money", impact: 300, iconName: "monetization_on" },
-    { name: "Supported NGOs", impact: 400, iconName: "diversity_4" },
-    { name: "Supporter causes", impact: 500, iconName: "interests" }
-  ]);
+  const { currentUser } = useCurrentUser();
+  const { userStatistics } = useStatistics({ userId: currentUser?.id });
+
+  const impacts = useCallback(() =>
+    [
+      { name: "Donated tickets", impact: userStatistics?.totalTickets ?? 0, iconName: "confirmation_number" },
+      { name: "Donated money", impact: userStatistics?.totalDonated?.brl ?? 0, iconName: "monetization_on" },
+      { name: "Supported NGOs", impact: userStatistics?.totalNonProfits ?? 0, iconName: "diversity_4" },
+      { name: "Supporter causes", impact: userStatistics?.totalCauses ?? 0, iconName: "interests" }
+    ], [userStatistics]);
 
   const renderItem = ({ name, impact, iconName }: { name: string, impact: number, iconName: string }) => (
     <ImpactCard
+      key={name}
       onPress={() => { }}
       description={name}
       impact={impact}
@@ -22,7 +29,7 @@ function ImpactCards(): JSX.Element {
 
   return (
     <View style={S.cardsContainer}>
-      {impacts.map((badge) => renderItem(badge))}
+      {impacts().map((impact) => renderItem(impact))}
     </View>
   );
 }
