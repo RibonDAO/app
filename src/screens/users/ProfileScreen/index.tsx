7bc848574316d1d2b38da4170e655830aef9b11a
Yan Galli <yangalli@hotmail.com>
2023-02-02 11:32:29 -0300

    feat: add translation for impact screens

--- src/screens/users/ProfileScreen/index.tsx
+++ src/screens/users/ProfileScreen/index.tsx
@@ -4,11 +4,16 @@ import { Text } from "components/Themed";
 import S from "./styles";
 import ImpactCards from "./ImpactCards";
 import NgoImpactCards from "./NgoImpactCards";
+import { useTranslation } from "react-i18next";
 
 function BadgesProfilePage() {
+  const { t } = useTranslation("translation", {
+    keyPrefix: "users.profileScreen",
+  });
+
   return (
     <ScrollView style={S.container} showsVerticalScrollIndicator={false}>
-      <Text style={S.title}>My Impact</Text>
+      <Text style={S.title}>{t("title")}</Text>
 
       <ImpactCards />
 
