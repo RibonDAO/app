e8650e7414218323cf8b07c398a7a1303c9297db
Nicholas Marques <devnicknish@gmail.com>
2023-02-13 17:36:28 -0300

    feat: add CardStories component

--- src/screens/users/ProfileScreen/index.tsx
+++ src/screens/users/ProfileScreen/index.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { ScrollView } from "react-native";
 import { Text } from "react-native";
+import { useTranslation } from "react-i18next";
+import CardStories from "components/moleculars/CardStories";
 import S from "./styles";
 import ImpactCards from "./ImpactCards";
 import NgoImpactCards from "./NgoImpactCards";
-import { useTranslation } from "react-i18next";
 
 function ProfileScreen() {
   const { t } = useTranslation("translation", {
     keyPrefix: "users.profileScreen",
   });
+
+  return <CardStories />;
 
   return (
     <ScrollView style={S.container} showsVerticalScrollIndicator={false}>
