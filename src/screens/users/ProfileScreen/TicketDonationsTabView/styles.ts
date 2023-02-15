22df0c880853f4e9efb9b9ea02a7e13aa1435144
Yan Galli <yangalli@hotmail.com>
2023-02-15 11:05:07 -0300

    feat: add translations to impact components

---
+++ src/screens/users/ProfileScreen/TicketDonationsTabView/styles.ts
@@ -0,0 +1,11 @@
+import { theme } from "@ribon.io/shared/styles";
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    backgroundColor: theme.colors.neutral10
+  }
+});
+
+export default styles;
