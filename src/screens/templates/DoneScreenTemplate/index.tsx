dcb09f26e58661054d197c43ead48b4ee9846364
Nicholas Marques <devnicknish@gmail.com>
2023-02-16 11:08:46 -0300

    feat: add done screen icons

--- src/screens/templates/DoneScreenTemplate/index.tsx
+++ src/screens/templates/DoneScreenTemplate/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { Button, View, Text } from "react-native";
 import Image from "components/atomics/Image";
 import { theme } from "@ribon.io/shared/styles";
+import Flare from "components/vectors/Flare";
+import Sparkles from "components/vectors/Sparkles";
+import ConfirmationNumber from "components/vectors/ConfirmationNumber";
+import VolunteerActivism from "components/vectors/VolunteerActivism";
 import S from "./styles";
 
 type Props = {
@@ -22,6 +26,10 @@ export default function DoneScreenTemplate({
 }: Props) {
   return (
     <View style={S.container}>
+      <Flare color="#8CE0BE" />
+      <Sparkles color="#8CE0BE" />
+      <ConfirmationNumber color="#8CE0BE" />
+      <VolunteerActivism color="#8CE0BE" />
       <View style={S.diamond}>
         {image && <Image style={S.cardImage} source={{ uri: image }} />}
       </View>
