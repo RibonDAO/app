26b60adee14a7a356954fda682643440f187255e
Nicholas Marques <devnicknish@gmail.com>
2023-02-16 15:05:58 -0300

    fix: update scroll view in profile page tabs

--- src/screens/users/ProfileScreen/CommunityDonationsTabView/index.tsx
+++ src/screens/users/ProfileScreen/CommunityDonationsTabView/index.tsx
@@ -1,12 +1,14 @@
-import { View } from "react-native";
+import { ScrollView } from "react-native";
 import { theme } from "@ribon.io/shared/styles";
 import CommunityDonationsImpactCards from "../CommunityDonationsImpactCards";
 
 function CommunityDonationsTabView(): JSX.Element {
   return (
-    <View style={{ height: "100%", backgroundColor: theme.colors.neutral10 }}>
+    <ScrollView
+      style={{ height: "100%", backgroundColor: theme.colors.neutral10 }}
+    >
       <CommunityDonationsImpactCards />
-    </View>
+    </ScrollView>
   );
 }
 
--- src/screens/users/ProfileScreen/DirectDonationsTabView/index.tsx
+++ src/screens/users/ProfileScreen/DirectDonationsTabView/index.tsx
@@ -1,12 +1,14 @@
-import { View } from "react-native";
+import { ScrollView } from "react-native";
 import { theme } from "@ribon.io/shared/styles";
 import DirectDonationsImpactCards from "../DirectDonationsImpactCards";
 
 function DirectDonationsTabView(): JSX.Element {
   return (
-    <View style={{ height: "100%", backgroundColor: theme.colors.neutral10 }}>
+    <ScrollView
+      style={{ height: "100%", backgroundColor: theme.colors.neutral10 }}
+    >
       <DirectDonationsImpactCards />
-    </View>
+    </ScrollView>
   );
 }
 
