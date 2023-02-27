71bbf2fe781c35194d67268f0998f51de7a13486
Nicholas Marques <devnicknish@gmail.com>
2023-02-27 15:35:13 -0300

    feat: add icons to DoneScreenTemplate background

--- src/screens/templates/DoneScreenTemplate/index.tsx
+++ src/screens/templates/DoneScreenTemplate/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Button, View, Text } from "react-native";
+import { Button, View, Text, ImageBackground } from "react-native";
 import Image from "components/atomics/Image";
 import { theme } from "@ribon.io/shared/styles";
+import GeneralIconsBackground from "assets/images/general-icons-background.png";
 import S from "./styles";
 
 type Props = {
@@ -22,9 +23,14 @@ export default function DoneScreenTemplate({
 }: Props) {
   return (
     <View style={S.container}>
-      <View style={S.diamond}>
-        {image && <Image style={S.cardImage} source={{ uri: image }} />}
-      </View>
+      <ImageBackground
+        source={GeneralIconsBackground}
+        style={S.diamondBackground}
+      >
+        <View style={S.diamond}>
+          {image && <Image style={S.cardImage} source={{ uri: image }} />}
+        </View>
+      </ImageBackground>
       {title && <Text style={S.title}>{title}</Text>}
       {description && (
         <Text style={S.description}>
