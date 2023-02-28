7f4b051f02cce46c768b7b3e69ee893fc9bc856b
Nicholas Marques <devnicknish@gmail.com>
2023-02-28 15:34:29 -0300

    test: fix scrollEnabledContext tests

--- src/contexts/scrollEnabledContext/index.test.tsx
+++ src/contexts/scrollEnabledContext/index.test.tsx
@@ -1,15 +1,34 @@
 import { renderComponent } from "config/testUtils/renders";
 import { expectTextToBeInTheDocument } from "config/testUtils/expects";
+import { Button, Text, View } from "react-native";
+import { clickOn } from "config/testUtils";
 import { useScrollEnabled } from ".";
 
 function ScrollEnabledTestPage() {
-  useScrollEnabled();
-  return <div>ScrollEnabled</div>;
+  const { setScrollEnabled, scrollEnabled } = useScrollEnabled();
+  return (
+    <View>
+      {scrollEnabled ? (
+        <Text>Scroll Enabled</Text>
+      ) : (
+        <Text>Scroll Disabled</Text>
+      )}
+      <Button title="Enable Scroll" onPress={() => setScrollEnabled(true)} />
+      <Button title="Disable Scroll" onPress={() => setScrollEnabled(false)} />
+    </View>
+  );
 }
 
 describe("useScrollEnabled", () => {
-  it("renders without error", () => {
+  it("starts enabled", () => {
     renderComponent(<ScrollEnabledTestPage />);
-    expectTextToBeInTheDocument("ScrollEnabled");
+    expectTextToBeInTheDocument("Scroll Enabled");
+  });
+
+  it("sets the scroll to disabled when the function is called", () => {
+    renderComponent(<ScrollEnabledTestPage />);
+    clickOn("Disable Scroll");
+
+    expectTextToBeInTheDocument("Scroll Disabled");
   });
 });
