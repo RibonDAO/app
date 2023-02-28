d4077e1ec02d89e5a36bf74ced45a1c965ce9cf9
Yan Galli <yangalli@hotmail.com>
2023-02-28 09:07:15 -0300

    fix: delete comment

--- src/components/moleculars/Onboarding/Slide/index.tsx
+++ src/components/moleculars/Onboarding/Slide/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Dimensions, Text, View } from "react-native";
 import Image from "components/atomics/Image";
 import S from "./styles";
-import ImpactDonationsVector1 from "./ImpactDonationsVector1";
 
 function Slide({ item }: any) {
   const { width } = Dimensions.get('window');
@@ -13,7 +12,6 @@ function Slide({ item }: any) {
         source={item?.image}
         style={{ ...S.image, width }}
       />
-      {/* <ImpactDonationsVector1 /> */}
 
       <View style={{ width, alignItems: "center" }}>
         <Text style={S.title}>{item?.title}</Text>
