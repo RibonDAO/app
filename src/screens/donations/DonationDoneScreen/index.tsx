b965819716ab9285e2e7e87fbbdfd346bafb9495
Yan Galli <yangalli@hotmail.com>
2023-03-02 09:19:24 -0300

    feat: add native sound

--- src/screens/donations/DonationDoneScreen/index.tsx
+++ src/screens/donations/DonationDoneScreen/index.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { RootStackScreenProps } from "types";
 import { useNavigation } from "hooks/useNavigation";
 import { useTranslation } from "react-i18next";
 import DoneScreenTemplate from "screens/templates/DoneScreenTemplate";
 import useFormattedImpactText from "hooks/useFormattedImpactText";
+import { Audio } from 'expo-av';
 
 export default function DonationDoneScreen({
   route,
@@ -11,14 +12,35 @@ export default function DonationDoneScreen({
   const { t } = useTranslation("translation", {
     keyPrefix: "donations.donationDoneScreen",
   });
+  const [sound, setSound] = useState<any>();
   const { nonProfit } = route.params;
   const { navigateTo } = useNavigation();
   const { formattedImpactText } = useFormattedImpactText();
+
+  const playSound = useCallback(async () => {
+    const { sound } = await Audio.Sound.createAsync(require('./assets/donation-done.mp3')
+    );
+    setSound(sound);
+
+    await sound.playAsync();
+  }, []);
+
+  useEffect(() => {
+    return sound
+      ? () => {
+        sound.unloadAsync();
+      }
+      : undefined;
+  }, [sound]);
 
   useEffect(() => {
     setTimeout(() => {
       navigateTo("ChooseCauseScreen");
     }, 5000);
+  }, []);
+
+  useEffect(() => {
+    playSound();
   }, []);
 
   return (
