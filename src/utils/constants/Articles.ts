72f5ea5120cf53615dbc662ec6ff44238a4b65ee
Henrique Barbosa <henrikleod@gmail.com>
2023-03-08 13:18:23 -0300

    fix: remove link from onboarding article

--- src/utils/constants/Articles.ts
+++ src/utils/constants/Articles.ts
@@ -3,16 +3,15 @@ import { Image } from "react-native";
 
 export const RibonOnboarding = (t: any) => ({
   id: 1,
-  title: t("onboarding.content"),
+  title: t("onboarding.title"),
   author: {
     id: 1,
     name: "Ribon",
   },
   visible: true,
   publishedAt: new Date().toISOString(),
-  publishedAtInWords: t("firstPost.date"),
+  publishedAtInWords: t("onboarding.publishedAt"),
   createdAt: "2021-09-01T00:00:00.000Z",
   updatedAt: "2021-09-01T00:00:00.000Z",
-  link: "https://ribon.io",
   imageUrl: Image.resolveAssetSource(OnboardingImage).uri,
 });
