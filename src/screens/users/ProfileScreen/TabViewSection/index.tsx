e542b3a3d3c10c7225eaffaedb6dc7416a5f17c9
Nicholas Marques <devnicknish@gmail.com>
2023-03-09 16:08:58 -0300

    feat: add collapsible tab view

--- src/screens/users/ProfileScreen/TabViewSection/index.tsx
+++ src/screens/users/ProfileScreen/TabViewSection/index.tsx
@@ -3,6 +3,7 @@ import { TabView, SceneMap, TabBar } from "react-native-tab-view";
 import { theme } from "@ribon.io/shared";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { CollapsibleTabView } from "react-native-collapsible-tab-view";
 import TicketDonationsTabView from "../TicketDonationsTabView";
 import CommunityDonationsTabView from "../CommunityDonationsTabView";
 import DirectDonationsTabView from "../DirectDonationsTabView";
@@ -42,25 +43,24 @@ function TabViewSection(): JSX.Element {
   const { t } = useTranslation("translation", {
     keyPrefix: "users.profileScreen",
   });
-
-  const layout = useWindowDimensions();
-
-  const [index, setIndex] = useState(0);
+  const [idx, setIdx] = useState(0);
   const [routes] = useState([
     { key: "TicketDonationsTabView", title: t("ticketDonationsTitle") },
     { key: "CommunityDonationsTabView", title: t("communityDonationsTitle") },
     { key: "DirectDonationsTabView", title: t("directDonationsTitle") },
   ]);
 
+  const handleIndexChange = (index: number) => {
+    setIdx(index);
+  };
+
   return (
     <View style={S.tabViewSection}>
-      <TabView
-        renderTabBar={renderTabBar}
-        navigationState={{ index, routes }}
+      <CollapsibleTabView<any>
+        navigationState={{ index: idx, routes }}
         renderScene={renderScene}
-        onIndexChange={setIndex}
-        initialLayout={{ width: layout.width }}
-        style={{ backgroundColor: theme.colors.neutral10 }}
+        onIndexChange={handleIndexChange}
+        disableSnap
       />
     </View>
   );
