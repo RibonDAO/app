a4713eac844d9a335b41b44836e704b3d8706099
Henrique Barbosa <henrikleod@gmail.com>
2023-04-03 14:32:02 -0300

    feat: add i18n to remaining parts

--- src/screens/content/ForYouScreen/TasksSection/index.tsx
+++ src/screens/content/ForYouScreen/TasksSection/index.tsx
@@ -1,21 +1,26 @@
 import { Text, View } from "react-native";
 import { useTasks } from "utils/constants/Tasks";
-import S from "./styles";
 import CheckBox from "components/atomics/inputs/Checkbox";
 import Icon from "components/atomics/Icon";
 import { theme } from "@ribon.io/shared";
 import ProgressBar from "components/atomics/ProgressBar";
+import { useTranslation } from "react-i18next";
+import S from "./styles";
 
 export default function TasksSection() {
+  const { t } = useTranslation("translation", {
+    keyPrefix: "content.forYouScreen.tasksSection",
+  });
+
   const dailyTasks = useTasks("daily");
-  const superTasks = useTasks("super_task");
+  // const superTasks = useTasks("super_task");
+
   return (
     <View style={S.container}>
       <View style={S.paddingContainer}>
         <View style={S.progressBar}>
           <ProgressBar value={1} min={0} max={dailyTasks.length} />
         </View>
-        <View style={S.timerWrapper}></View>
         <View style={S.titleContainer}>
           <Icon
             type="outlined"
@@ -23,11 +28,11 @@ export default function TasksSection() {
             size={25}
             color={theme.colors.brand.primary[900]}
           />
-          <Text style={S.title}>Tarefas</Text>
+          <Text style={S.title}>{t("title")}</Text>
         </View>
         {dailyTasks.map((task) => (
           <CheckBox
-            text={task.title}
+            text={t(`tasks.${task.title}`)}
             sectionStyle={{ marginBottom: 8, paddingLeft: 4 }}
             lineThroughOnChecked={true}
             navigationCallback={task?.navigationCallback}
--- src/utils/constants/Tasks.ts
+++ src/utils/constants/Tasks.ts
@@ -1,36 +1,29 @@
+/*
+* NOTE: The tasks below doesn't exist at a databse level, we're only saving the UUIDs when a task
+* is completed. This will help us to split tasks or not between clients, and give us more freedom to set callbacks and more.
+*/
+
 export const TASKS = [
   {
-    id: 1,
-    title: "Doar ticket",
+    id: "af13a631-3d3d-4709-8f90-06a938f4cef6",
+    title: "donate_ticket",
     actions: ["donation_done_page_view"],
     type: "daily",
     navigationCallback: "CausesScreen",
   },
   {
-    id: 2,
-    title: "Conferir publicações diárias",
+    id: "573d941a-1429-4095-adc3-ccb36f058633",
+    title: "check_daily_news",
     navigationCallback: "NewsSectionTabView",
     actions: ["news_page_view"],
     type: "daily",
   },
   {
-    id: 3,
-    title: "Ver mais informações sobre uma ONG",
+    id: "2825f25d-46c8-4307-a202-c41ecacfbfed",
+    title: "see_more_ngo_infos",
     actions: ["stories_page_view"],
     type: "daily",
     navigationCallback: "CausesScreen",
-  },
-  {
-    id: 4,
-    title: "Fazer doação direta",
-    actions: ["direct_donation_done"],
-    type: "super_task",
-  },
-  {
-    id: 5,
-    title: "Doar em comunidade",
-    actions: ["community_donation_done"],
-    type: "super_task",
   },
 ];
 
