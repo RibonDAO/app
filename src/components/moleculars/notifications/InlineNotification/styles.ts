0f11a2898931cddbf8458ff009132a66f981d570
Nicholas Marques <devnicknish@gmail.com>
2023-04-19 11:41:05 -0300

    refactor: add spacing native to styles

--- src/components/moleculars/notifications/InlineNotification/styles.ts
+++ src/components/moleculars/notifications/InlineNotification/styles.ts
@@ -9,7 +9,7 @@ import { theme } from "@ribon.io/shared/styles";
 const styles = StyleSheet.create({
   container: {
     width: "100%",
-    padding: 16,
+    padding: theme.spacingNative(16),
     borderWidth: 1,
     borderStyle: "solid",
     borderRadius: 8,
