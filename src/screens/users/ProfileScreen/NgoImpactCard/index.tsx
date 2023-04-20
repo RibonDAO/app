09df4c6a303b8a37b1364a3fc956931dc1e7f10d
Nicholas Marques <devnicknish@gmail.com>
2023-04-20 13:57:11 -0300

    feat: add Legacy impact to profile page

--- src/screens/users/ProfileScreen/NgoImpactCard/index.tsx
+++ src/screens/users/ProfileScreen/NgoImpactCard/index.tsx
@@ -7,9 +7,10 @@ export type Props = {
   name: string;
   icon: string;
   description?: string | JSX.Element;
+  label?: string;
 };
 
-function NgoImpactCard({ icon, name, description }: Props): JSX.Element {
+function NgoImpactCard({ icon, name, description, label }: Props): JSX.Element {
   return (
     <View style={{ marginBottom: theme.spacingNative(12) }}>
       <CardImageText
@@ -18,6 +19,7 @@ function NgoImpactCard({ icon, name, description }: Props): JSX.Element {
         text={description}
         subtitleStyle={S.impact}
         textStyle={S.description}
+        label={label}
       />
     </View>
   );
