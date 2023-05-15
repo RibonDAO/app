7060111db48e95f752963ca1ccd2f80cf7541c80
Yan Galli <yangalli@hotmail.com>
2023-05-15 10:44:33 -0300

    feat: add pagination to direct donations

--- src/screens/users/ProfileScreen/DirectDonationsImpactCards/index.tsx
+++ src/screens/users/ProfileScreen/DirectDonationsImpactCards/index.tsx
@@ -1,5 +1,5 @@
 import { View } from "react-native";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useNavigation } from "hooks/useNavigation";
 import { useTranslation } from "react-i18next";
 import usePersonPayments from "hooks/apiHooks/usePersonPayments";
@@ -7,19 +7,47 @@ import { theme } from "@ribon.io/shared/styles";
 import DirectDonationCard from "screens/users/ProfileScreen/DirectDonationsImpactCards/DirectDonationCard";
 import { logEvent } from "services/analytics";
 import { useFocusEffect } from "@react-navigation/native";
+import Button from "components/atomics/buttons/Button";
 import ImpactDonationsVector from "./ImpactDonationsVector";
 import S from "./styles";
 import ZeroDonationsSection from "../ZeroDonationsSection";
 
 function DirectDonationsImpactCards(): JSX.Element {
   const { useDirectPersonPayments } = usePersonPayments();
+  const [page, setPage] = useState(1);
+  const per = 2;
+  const [showMoreDisabled, setShowMoreDisabled] = useState(false);
+  const [showMoreVisible, setShowMoreVisible] = useState(true);
+  const { data, refetch } = useDirectPersonPayments(page, per);
+  const [impactCards, setImpactCards] = useState<any>([]);
 
-  const { data, refetch } = useDirectPersonPayments(1, 6);
+  const hasDuplicatedIds = (items: any[]) => {
+    const existentIds = new Set(impactCards.map((obj: any) => obj.id));
+    const newIds = items.map((obj: any) => obj.id);
+
+    return newIds.some((id) => existentIds.has(id));
+  };
 
   useFocusEffect(
     useCallback(() => {
+      if (!data) return;
+
+      if (data.length === 0) {
+        setShowMoreVisible(false);
+        return;
+      }
+
+      if (page === 1) {
+        setImpactCards(data);
+      } else if (!hasDuplicatedIds(data) && page > 1) {
+        setImpactCards((items: any) => [...items, ...data]);
+      }
+
+      setShowMoreDisabled(false);
+      if (data.length < per) setShowMoreVisible(false);
+
       refetch();
-    }, []),
+    }, [data, page]),
   );
 
   const impactItems = useCallback(() => data || [], [data]);
@@ -30,17 +58,32 @@ function DirectDonationsImpactCards(): JSX.Element {
   });
 
   const navigateToPromotersScreen = () => {
-    logEvent("giveNonProfitCard_click", { from: "impactEmptystate" });
+    logEvent("giveNonProfitCard_click", { from: "impactEmptyState" });
     navigateTo("PromotersScreen", { isInCommunity: false });
+  };
+
+  const handleShowMoreClick = () => {
+    setPage(page + 1);
+    setShowMoreDisabled(true);
   };
 
   const impactCardsList = () => (
     <View style={S.cardsContainer}>
-      {impactItems()?.map((item) => (
+      {impactCards?.map((item: any) => (
         <View key={item?.id} style={{ marginBottom: theme.spacingNative(12) }}>
           <DirectDonationCard personPayment={item} />
         </View>
       ))}
+      {showMoreVisible && (
+        <View style={S.showMoreButtonContainer}>
+          <Button
+            outline
+            text={t("showMore")}
+            onPress={handleShowMoreClick}
+            disabled={showMoreDisabled}
+          />
+        </View>
+      )}
     </View>
   );
 
