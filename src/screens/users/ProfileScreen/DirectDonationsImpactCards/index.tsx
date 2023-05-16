063c11ee663aafe8d748bd1a74e05bd66ada9b98
Yan Galli <yangalli@hotmail.com>
2023-05-16 11:15:30 -0300

    feat: add loader to direct impact pagination

--- src/screens/users/ProfileScreen/DirectDonationsImpactCards/index.tsx
+++ src/screens/users/ProfileScreen/DirectDonationsImpactCards/index.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import { useCallback, useState } from "react";
 import { useNavigation } from "hooks/useNavigation";
 import { useTranslation } from "react-i18next";
@@ -8,18 +8,20 @@ import DirectDonationCard from "screens/users/ProfileScreen/DirectDonationsImpac
 import { logEvent } from "services/analytics";
 import { useFocusEffect } from "@react-navigation/native";
 import Button from "components/atomics/buttons/Button";
+import LoaderAnimated from "components/atomics/LoaderAnimated";
 import ImpactDonationsVector from "./ImpactDonationsVector";
 import S from "./styles";
 import ZeroDonationsSection from "../ZeroDonationsSection";
 
 function DirectDonationsImpactCards(): JSX.Element {
   const { useDirectPersonPayments } = usePersonPayments();
-  const per = 6;
+  const per = 2;
   const [page, setPage] = useState(1);
   const [showMoreDisabled, setShowMoreDisabled] = useState(false);
   const [showMoreVisible, setShowMoreVisible] = useState(true);
   const { data, refetch } = useDirectPersonPayments(page, per);
   const [impactCards, setImpactCards] = useState<any>([]);
+  const [loading, setLoading] = useState(false);
 
   const impactItems = useCallback(() => data || [], [data]);
   const hasImpact = impactItems() && impactItems()?.length > 0;
@@ -63,42 +65,68 @@ function DirectDonationsImpactCards(): JSX.Element {
   };
 
   const handleShowMoreClick = () => {
+    setLoading(true);
+
     setPage(page + 1);
     setShowMoreDisabled(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
   };
 
-  const impactCardsList = () => (
-    <View style={S.cardsContainer}>
-      {impactCards?.map((item: any) => (
-        <View key={item?.id} style={{ marginBottom: theme.spacingNative(12) }}>
-          <DirectDonationCard personPayment={item} />
-        </View>
-      ))}
+  function renderZeroDonationsSection() {
+    if (loading) {
+      return <Text />;
+    } else {
+      return (
+        <ZeroDonationsSection
+          title={t("direct.title")}
+          onButtonPress={navigateToPromotersScreen}
+          description={t("direct.description")}
+          buttonText={t("direct.buttonText")}
+          image={<ImpactDonationsVector />}
+        />
+      );
+    }
+  }
 
-      {showMoreVisible && (
-        <View style={S.showMoreButtonContainer}>
-          <Button
-            outline
-            text={t("showMore")}
-            onPress={handleShowMoreClick}
-            disabled={showMoreDisabled}
-          />
-        </View>
-      )}
-    </View>
-  );
+  function renderLoadingAnimation() {
+    return (
+      <View style={S.loaderContainer}>
+        <LoaderAnimated width={160} height={160} speed={1.5} />
+      </View>
+    );
+  }
 
-  return hasImpact ? (
-    impactCardsList()
-  ) : (
-    <ZeroDonationsSection
-      title={t("direct.title")}
-      onButtonPress={navigateToPromotersScreen}
-      description={t("direct.description")}
-      buttonText={t("direct.buttonText")}
-      image={<ImpactDonationsVector />}
-    />
-  );
+  const impactCardsList = () =>
+    loading ? (
+      renderLoadingAnimation()
+    ) : (
+      <View style={S.cardsContainer}>
+        {impactCards?.map((item: any) => (
+          <View
+            key={item?.id}
+            style={{ marginBottom: theme.spacingNative(12) }}
+          >
+            <DirectDonationCard personPayment={item} />
+          </View>
+        ))}
+
+        {showMoreVisible && (
+          <View style={S.showMoreButtonContainer}>
+            <Button
+              outline
+              text={t("showMore")}
+              onPress={handleShowMoreClick}
+              disabled={showMoreDisabled}
+            />
+          </View>
+        )}
+      </View>
+    );
+
+  return hasImpact ? impactCardsList() : renderZeroDonationsSection();
 }
 
 export default DirectDonationsImpactCards;
