31ea9e3d1f08802559ce38791ee122b346c7b4ff
Nicholas Marques <devnicknish@gmail.com>
2023-07-07 15:04:30 -0300

    refactor: add focus effect in for you page to reload can donate

--- src/screens/content/ForYouScreen/index.tsx
+++ src/screens/content/ForYouScreen/index.tsx
@@ -1,28 +1,26 @@
 import { View } from "react-native";
 import { useCanDonate } from "@ribon.io/shared";
 import { PLATFORM, RIBON_INTEGRATION_ID } from "utils/constants/Application";
-import { useCurrentUser } from "contexts/currentUserContext";
-import { useEffect } from "react";
+import { useCallback } from "react";
 import ForYouTabsProvider from "contexts/forYouTabsContext";
 import { useRouteParams } from "hooks/useRouteParams";
+import { useFocusEffect } from "@react-navigation/native";
 import TabViewSection from "./TabViewSection";
 import styles from "./styles";
 
 export default function ForYouScreen(): JSX.Element {
   const { params } = useRouteParams<"ForYouScreen">();
 
-  const { currentUser } = useCurrentUser();
-
   const { refetch: refetchCanDonate } = useCanDonate(
     RIBON_INTEGRATION_ID,
     PLATFORM,
   );
 
-  useEffect(() => {
-    setTimeout(() => {
+  useFocusEffect(
+    useCallback(() => {
       refetchCanDonate();
-    }, 500);
-  }, [JSON.stringify(currentUser)]);
+    }, []),
+  );
 
   return (
     <ForYouTabsProvider>
