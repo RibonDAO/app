b2d8f3028a6ed10e93039fc4f722a53aeb3b919e
Henrique Barbosa <henrikleod@gmail.com>
2023-07-17 01:20:19 -0300

    chore: add usepayable

---
+++ src/hooks/usePayable/index.test.ts
@@ -0,0 +1,31 @@
+import { renderHook } from "@testing-library/react-hooks";
+import { causeFactory, nonProfitFactory } from "@ribon.io/shared";
+import usePayable from ".";
+
+const mockCause = causeFactory({ name: "💊 Health", id: 1 });
+const mockNonProfit = nonProfitFactory({ name: "🌳 Environment", id: 1 });
+
+jest.mock("@ribon.io/shared/hooks", () => ({
+  __esModule: true,
+  ...jest.requireActual("@ribon.io/shared/hooks"),
+  useCauses: () => ({
+    causes: [mockCause],
+    refetch: jest.fn(),
+  }),
+  useNonProfits: () => ({
+    nonProfits: [mockNonProfit],
+    refetch: jest.fn(),
+  }),
+}));
+
+describe("usePayable", () => {
+  it("should return a cause", () => {
+    const { result } = renderHook(() => usePayable("cause", "1"));
+    expect(result.current).toEqual(mockCause);
+  });
+
+  it("should return a non profit", () => {
+    const { result } = renderHook(() => usePayable("non_profit", "1"));
+    expect(result.current).toEqual(mockNonProfit);
+  });
+});
