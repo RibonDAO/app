8388edf17c05fa2932e1bc0b60aca9966ff75456
Nicholas Marques <devnicknish@gmail.com>
2023-07-17 18:01:02 -0300

    refactor: add empty object to default usePageView param

--- src/hooks/usePageView/index.tsx
+++ src/hooks/usePageView/index.tsx
@@ -2,15 +2,10 @@ import { useFocusEffect } from "@react-navigation/native";
 import { useCallback } from "react";
 import { logEvent } from "services/analytics";
 
-function usePageView(eventName: string, eventParams?: Record<any, any>) {
+function usePageView(eventName: string, eventParams = {}) {
   useFocusEffect(
     useCallback(() => {
-      if (eventParams) {
-        logEvent(eventName, eventParams);
-        return;
-      }
-
-      logEvent(eventName);
+      logEvent(eventName, eventParams);
     }, []),
   );
 }
