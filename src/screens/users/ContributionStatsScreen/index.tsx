b28061ba99cb7253b9dc86394df39269f3f0357f
Nicholas Marques <devnicknish@gmail.com>
2023-07-19 17:06:29 -0300

    refactor: add OpinionSection to ContributionStatsScreen

--- src/screens/users/ContributionStatsScreen/index.tsx
+++ src/screens/users/ContributionStatsScreen/index.tsx
@@ -1,17 +1,14 @@
 import React from "react";
-import { View, Text, ScrollView, Linking } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import { useContributions } from "@ribon.io/shared/hooks";
 import { useTranslation } from "react-i18next";
-import { logEvent } from "services/analytics";
 import { Loader } from "rn-placeholder";
 import { useCurrentUser } from "contexts/currentUserContext";
 import { useRouteParams } from "hooks/useRouteParams";
 import VideoSection from "screens/users/ContributionStatsScreen/VideoSection";
-import Banner from "components/moleculars/Banner";
-import { theme } from "@ribon.io/shared/styles";
-import RibonSunLeft from "assets/images/ribon-sun-left.png";
 import GiftCycleSection from "screens/users/ContributionStatsScreen/GiftCycleSection";
 import usePageView from "hooks/usePageView";
+import OpinionSection from "screens/users/ContributionStatsScreen/OpinionSection";
 import EngagementSection from "./EngagementSection";
 import BoostSection from "./BoostSection";
 import S from "./styles";
@@ -54,31 +51,7 @@ function ContributionStatsScreen(): JSX.Element {
           <BoostSection totalAmountToCause={data.stats.totalAmountToCause} />
           <View style={S.divider} />
           <VideoSection />
-
-          <View style={S.bannerContainer}>
-            <Banner
-              icon={{
-                name: "support_agent",
-                color: theme.colors.neutral[900],
-                type: "rounded",
-                size: 20,
-              }}
-              title={{
-                text: t("supportSection.title"),
-                color: theme.colors.neutral[900],
-              }}
-              text={t("supportSection.text").toString()}
-              textColor={theme.colors.neutral[800]}
-              arrowLinkColor={theme.colors.brand.secondary[800]}
-              onArrowClick={() => {
-                logEvent("P24_opinionCardBtn_click");
-                Linking.openURL(
-                  "https://api.whatsapp.com/send/?phone=554896605461&text=Oi%2C+quero+compartilhar+minha+opini%C3%A3o+:)&type=phone_number&app_absent=0",
-                );
-              }}
-              cardBackground={RibonSunLeft}
-            />
-          </View>
+          <OpinionSection />
         </View>
       </View>
     </ScrollView>
