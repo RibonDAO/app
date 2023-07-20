09ce23fac14d343f699ce4d96deae14cdc0e5247
Thais Correa <thais@ribon.io>
2023-07-20 07:54:56 -0300

    feat: deep link

--- src/services/deepLink/index.ts
+++ src/services/deepLink/index.ts
@@ -1,5 +1,8 @@
+import { setLocalStorageItem } from "@ribon.io/shared";
 import branch from "react-native-branch";
+import { RIBON_INTEGRATION_ID } from "utils/constants/Application";
 
+export const INTEGRATION_ID = "integration_id";
 export async function initializeDeeplink(navigateTo: (path: string) => void) {
   branch.subscribe({
     onOpenStart: ({ uri, cachedInitialEvent }) => {
@@ -9,12 +12,15 @@ export async function initializeDeeplink(navigateTo: (path: string) => void) {
       );
     },
     onOpenComplete: async ({ error, params, uri }) => {
+      const latestParams = await branch.getLatestReferringParams();
+      const integrationId =
+        (latestParams.$integration_id as string) || RIBON_INTEGRATION_ID;
+      setLocalStorageItem(INTEGRATION_ID, integrationId);
       if (error) {
         // eslint-disable-next-line no-console
         console.error(
           `subscribe onOpenComplete, Error from opening uri: ${uri} error: ${error}`,
         );
-        const latestParams = await branch.getLatestReferringParams();
         if (latestParams.$custom_meta_tags)
           navigateTo(latestParams.$custom_meta_tags as string);
         return;
