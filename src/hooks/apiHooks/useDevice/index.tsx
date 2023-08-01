e05c83b797b00b02d12f21003b0eaa7b0b537383
Juju <juju@ribon.io>
2023-08-01 15:24:14 -0300

    Remove api hook

--- src/hooks/apiHooks/useDevice/index.tsx
+++ src/hooks/apiHooks/useDevice/index.tsx
@@ -1,37 +1,26 @@
-import { useApi } from "@ribon.io/shared/hooks";
-import { emptyRequest } from "@ribon.io/shared/services";
 import { useCurrentUser } from "contexts/currentUserContext";
 import * as DeviceInfo from "expo-device";
 import deviceApi from "services/api/deviceApi";
-import { Device } from "types/entities/Device";
-import { useCallback } from "react";
 import { getNotificationToken } from "lib/notifications";
+import { logError } from "services/crashReport";
 
 function useDevices() {
   const { currentUser } = useCurrentUser();
 
-  const registerDevice = useCallback(() => {
-    const { data, error, isLoading, refetch } = useApi<Device>({
-      key: "registerDevices",
-      fetchMethod: async () => {
-        const notificationToken = await getNotificationToken();
-        if (!currentUser?.id) return emptyRequest();
+  const registerDevice = async () => {
+    const notificationToken = await getNotificationToken();
+    if (!currentUser?.id) return;
 
-        return deviceApi.registerDevice({
-          deviceId: DeviceInfo.deviceName,
-          deviceToken: notificationToken,
-          userId: currentUser.id,
-        });
-      },
-    });
-
-    return {
-      data,
-      error,
-      isLoading,
-      refetch,
-    };
-  }, [currentUser?.id]);
+    try {
+      deviceApi.registerDevice({
+        deviceId: DeviceInfo.deviceName,
+        deviceToken: notificationToken,
+        userId: currentUser.id,
+      });
+    } catch (e) {
+      logError(e);
+    }
+  };
 
   return {
     registerDevice,
