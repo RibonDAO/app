d104aea18e2671cd51a4c0959da62cd9acb753e2
Nathiely Macedo <nathielymm@gmail.com>
2023-10-03 19:57:52 -0300

    remove duplicated changes

--- src/screens/users/ContributionStatsScreen/VideoSection/index.tsx
+++ src/screens/users/ContributionStatsScreen/VideoSection/index.tsx
@@ -1,19 +1,12 @@
 import { Text, View } from "react-native";
 import { useTranslation } from "react-i18next";
 import { WebView } from "react-native-webview";
-import { useLanguage } from "contexts/languageContext";
-import { Languages } from "@ribon.io/shared/types";
 import S from "./styles";
 
 function VideoSection() {
   const { t } = useTranslation("translation", {
     keyPrefix: "contributionStatsPage.video",
   });
-  const { currentLang } = useLanguage();
-  const ptEmbedId = "tJ9lY9npcNU";
-  const enEmbedId = "7hgatm9cq40";
-  const embedId = currentLang === Languages.PT ? ptEmbedId : enEmbedId;
-
   return (
     <View style={S.container}>
       <Text>
@@ -25,7 +18,7 @@ function VideoSection() {
         style={{ opacity: 0.99 }} // opacity is set to 0.99 to avoid a bug in the WebView component
         javaScriptEnabled
         source={{
-          uri: `https://www.youtube.com/embed/${embedId}?rel=0&autoplay=0&showinfo=0&controls=0`,
+          uri: "https://www.youtube.com/embed/tJ9lY9npcNU?rel=0&autoplay=0&showinfo=0&controls=0",
         }}
       />
     </View>
