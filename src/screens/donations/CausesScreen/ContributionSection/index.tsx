bb7976d7cca6de9ca6aab9dc126560a01a39e04a
Henrique Barbosa <henrikleod@gmail.com>
2023-10-31 17:03:13 -0300

    chore: wait for promise

--- src/screens/donations/CausesScreen/ContributionSection/index.tsx
+++ src/screens/donations/CausesScreen/ContributionSection/index.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import { useTranslation } from "react-i18next";
 import { useImpactConversion } from "hooks/useImpactConversion";
 import CardCampaign from "components/moleculars/CardCampaign";
-import { useImpressionCards } from "@ribon.io/shared";
+import { ImpressionCard, useImpressionCards } from "@ribon.io/shared";
+import { logError } from "services/crashReport";
 import S from "./styles";
 
 export default function ContributionSection() {
@@ -14,11 +15,24 @@ export default function ContributionSection() {
   const CURRENT_CARD_ID = 2;
 
   const { nonProfit } = useImpactConversion();
+  const [impressionCard, setImpressionCard] = useState<ImpressionCard | null>();
+
   const { getImpressionCard } = useImpressionCards();
 
-  const hasImpressionCard = Boolean(getImpressionCard(CURRENT_CARD_ID));
+  const fetchImpressionCard = async () => {
+    try {
+      const impressionCardData = await getImpressionCard(CURRENT_CARD_ID);
+      setImpressionCard(impressionCardData);
+    } catch (e) {
+      logError(e);
+    }
+  };
 
-  return hasImpressionCard ? (
+  useEffect(() => {
+    fetchImpressionCard();
+  }, []);
+
+  return impressionCard ? (
     <>
       <View style={S.container}>
         <Text style={S.title}>
