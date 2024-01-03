1ec38d00482589724858cba6e05062465896ee39
Thais Correa <thais@ribon.io>
2024-01-03 11:34:27 -0300

    feat: fix style height

--- src/screens/auth/ExtraTicketScreen/styles.ts
+++ src/screens/auth/ExtraTicketScreen/styles.ts
@@ -42,7 +42,7 @@ export const Button = styled(ButtonComponent)`
 
 export const Container = styled.View`
   width: 100%;
-  height: 100vh;
+  height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
