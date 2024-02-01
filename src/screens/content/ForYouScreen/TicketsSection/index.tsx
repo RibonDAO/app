5a6c71f6a99159abc40d4528f7e734e42f565057
mendesmcmg <claraplanejamento@gmail.com>
2024-02-01 10:03:35 -0300

    feat: adds tickets section to for you screen

---
+++ src/screens/content/ForYouScreen/TicketsSection/index.tsx
@@ -0,0 +1,42 @@
+import { theme } from "@ribon.io/shared";
+import TicketIllustration from "assets/illustrations/TicketIllustration";
+import Button from "components/atomics/buttons/Button";
+import CardTicket from "components/moleculars/CardTicket";
+import TicketWhiteIcon from "components/vectors/TicketWhiteIcon";
+import { useTranslation } from "react-i18next";
+import { View } from "react-native";
+import S from "./styles";
+
+export default function TicketsSection() {
+  const { t } = useTranslation("translation", {
+    keyPrefix: "content.forYouScreen.ticketsSection",
+  });
+
+  return (
+    <View style={S.container}>
+      <CardTicket
+        title={t("dailyTicketCard.title")}
+        subtitle={{
+          icon: <TicketWhiteIcon />,
+          text: t("dailyTicketCard.subtitle"),
+          color: theme.colors.brand.primary[900],
+        }}
+        icon={<TicketIllustration />}
+      >
+        <Button
+          text={t("dailyTicketCard.buttonText")}
+          textColor={theme.colors.brand.primary[600]}
+          borderColor={theme.colors.brand.primary[50]}
+          backgroundColor={theme.colors.brand.primary[50]}
+          leftIcon={{
+            name: "check",
+            color: theme.colors.brand.primary[600],
+            type: "outlined",
+            size: 24,
+          }}
+          onPress={() => {}}
+        />
+      </CardTicket>
+    </View>
+  );
+}
