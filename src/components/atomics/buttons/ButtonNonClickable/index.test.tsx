c6de42b04bf6818aa4bdd865a483f166a0754ac0
mendesmcmg <claraplanejamento@gmail.com>
2024-02-01 16:10:43 -0300

    fix: removes clicking from test

--- src/components/atomics/buttons/ButtonNonClickable/index.test.tsx
+++ src/components/atomics/buttons/ButtonNonClickable/index.test.tsx
@@ -1,21 +1,13 @@
 import { renderComponentAsync } from "config/testUtils/renders";
 import { expectTextToBeInTheDocument } from "config/testUtils/expects";
-import { clickOn } from "config/testUtils";
 import Button from ".";
 
 describe("Button", () => {
-  const mockFn = jest.fn();
-
   beforeEach(async () => {
     await renderComponentAsync(<Button text="text" />);
   });
 
   it("renders without error", () => {
     expectTextToBeInTheDocument("text");
-  });
-
-  it("calls the mockFn when clicked", () => {
-    clickOn("text");
-    expect(mockFn).toHaveBeenCalled();
   });
 });
