aa98fb2156161410acca8e5acfd8fa29a620cb92
Nathiely Macedo <nathielymm@gmail.com>
2024-02-22 17:17:25 -0300

    feat: adds card checkbox with tests

---
+++ src/components/moleculars/GroupCardsCheckbox/index.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import CardCheckbox from "../CardCheckbox";
+import * as S from "./styles";
+
+type GroupCardsCheckboxProps = {
+  elements: any[];
+  onChange?: (element: any, index: number) => void;
+  indexSelected?: number;
+};
+
+function GroupCardsCheckbox({
+  elements,
+  onChange,
+  indexSelected,
+}: GroupCardsCheckboxProps): JSX.Element {
+  const [selectedButtonIndex, setSelectedButtonIndex] = useState(
+    indexSelected || 0,
+  );
+
+  const handleElementClick = (index: number, element: any) => {
+    setSelectedButtonIndex(index);
+    if (onChange) onChange(element, index);
+  };
+
+  function renderGroupCardsCheckbox() {
+    return elements.map((element: any, index: number) => (
+      <S.Button
+        onClick={() => handleElementClick(index, element)}
+        /* eslint-disable react/no-array-index-key */
+        key={index.toString()}
+        type="button"
+        style={{ border: "none", background: "none" }}
+        testID={`card-checkbox-${index}`}
+      >
+        <CardCheckbox
+          firstDescription={element.firstDescription}
+          firstIconName={element.firstIconName}
+          secondDescription={element.secondDescription}
+          secondIconName={element.secondIconName}
+          value={element.value}
+          recurrence={element.recurrence}
+          tagText={element.tagText}
+          onClick={() => handleElementClick(index, element)}
+          checked={index === selectedButtonIndex}
+        />
+      </S.Button>
+    ));
+  }
+
+  return <S.Container>{renderGroupCardsCheckbox()}</S.Container>;
+}
+
+export default GroupCardsCheckbox;
