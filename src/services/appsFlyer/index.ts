0bc7a4dca288ebae941a603061810bc77ff0b279
Henrique Barbosa <henrikleod@gmail.com>
2024-03-18 10:08:24 -0300

    fix: use expo secrets to handle appsflyer env vars

--- src/services/appsFlyer/index.ts
+++ src/services/appsFlyer/index.ts
@@ -1,12 +1,15 @@
 import appsFlyer from "react-native-appsflyer";
 import { logEvent } from "services/analytics";
 import { logError } from "services/crashReport";
-import { EXPO_PUBLIC_APPSFLYER_APP_ID } from "utils/constants/Application";
+import {
+  EXPO_PUBLIC_APPSFLYER_APP_ID,
+  EXPO_PUBLIC_APPSFLYER_KEY,
+} from "utils/constants/Application";
 
 export async function initAppsFlyer() {
   appsFlyer.initSdk(
     {
-      devKey: process.env.REACT_APP_APPSFLYER_KEY || "",
+      devKey: EXPO_PUBLIC_APPSFLYER_KEY,
       isDebug: false,
       appId: EXPO_PUBLIC_APPSFLYER_APP_ID,
       onInstallConversionDataListener: true,
