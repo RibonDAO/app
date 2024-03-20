b0210a781dd141b0363fcf28bce36836e32c638c
Nathiely Macedo <nathielymm@gmail.com>
2024-03-20 15:01:35 -0300

    feat: adds manage subscription events

--- src/screens/promoters/SubscriptionsScreen/CancelSubscriptionModal/index.tsx
+++ src/screens/promoters/SubscriptionsScreen/CancelSubscriptionModal/index.tsx
@@ -1,6 +1,7 @@
 import { useSubscriptions } from "@ribon.io/shared/hooks";
 import ModalDialog from "components/moleculars/modals/ModalDialog";
 import { showToast } from "lib/Toast";
+import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { logEvent } from "services/analytics";
 import { logError } from "services/crashReport";
@@ -8,8 +9,9 @@ import { logError } from "services/crashReport";
 type Props = {
   visible: boolean;
   setVisible: (visible: boolean) => void;
-  club: boolean;
   subscriptionId: string | number;
+  club?: boolean;
+  eventParams?: any;
 };
 
 function CancelSubscriptionModal({
@@ -17,6 +19,7 @@ function CancelSubscriptionModal({
   setVisible,
   subscriptionId,
   club = false,
+  eventParams,
 }: Props): JSX.Element {
   const { t } = useTranslation("translation", {
     keyPrefix: "promoters.subscriptionsScreen.cancelSubscriptionModal",
@@ -24,12 +27,15 @@ function CancelSubscriptionModal({
 
   const { sendCancelSubscriptionEmail } = useSubscriptions();
 
+  useEffect(() => {
+    logEvent("cancelSubsModal_view", eventParams);
+  }, []);
+
   const handleCancelSubscription = async () => {
     if (!subscriptionId) {
       return;
     }
-
-    logEvent("cancelSubs_click");
+    logEvent("cancelSubsBtn_click", eventParams);
     try {
       const response = await sendCancelSubscriptionEmail(subscriptionId);
       if (response) {
