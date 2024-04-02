8bbdc41e91a816d2b9674767ff0a1d32a8b4e00f
Thais Correa <thais@ribon.io>
2024-04-02 12:36:54 -0300

    feat: tests

--- src/config/testUtils/setupTests.ts
+++ src/config/testUtils/setupTests.ts
@@ -23,6 +23,13 @@ function setupMocks() {
     __esModule: true,
     default: () => true,
   }));
+  jest.mock("@ribon.io/shared", () => {
+    const original = jest.requireActual("@ribon.io/shared");
+    return {
+      ...original,
+      useUploadFile: jest.fn(),
+    };
+  });
   jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
 }
 
