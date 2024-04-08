def03f166f543cf4511f572ef016e78e0078e353
mendesmcmg <claraplanejamento@gmail.com>
2024-04-08 18:57:02 -0300

    fix: adjusts styles for ios

--- src/components/moleculars/CardCenterImageButton/styles.ts
+++ src/components/moleculars/CardCenterImageButton/styles.ts
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { Platform, StyleSheet } from "react-native";
 import { theme } from "@ribon.io/shared/styles";
 import {
   defaultBodyMdRegular,
@@ -119,7 +119,7 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
-    height: 24,
+    height: Platform.OS !== "ios" ? 24 : "auto",
   },
   iconTextContainer: {
     display: "flex",
