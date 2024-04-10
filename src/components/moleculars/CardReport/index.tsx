259b5170940f151807474ef774f37f8308cb828d
Jean Scalabrin <jean.scalabrin@koud.com.br>
2024-04-10 10:27:02 -0300

    chore: add events in report cards

--- src/components/moleculars/CardReport/index.tsx
+++ src/components/moleculars/CardReport/index.tsx
@@ -2,19 +2,34 @@ import { Text, TouchableOpacity, View } from "react-native";
 import Icon from "components/atomics/Icon";
 import { theme } from "@ribon.io/shared/styles";
 import { openInWebViewer } from "lib/linkOpener";
+import { logEvent } from "services/analytics";
 import S from "./styles";
 
 export type Props = {
   title: string;
   link: string;
   showIcon: boolean;
+  clickEventName?: string;
 };
 
-function CardReport({ title, link, showIcon }: Props): JSX.Element {
+function CardReport({
+  title,
+  link,
+  showIcon,
+  clickEventName,
+}: Props): JSX.Element {
+  const handleOnPress = () => {
+    if (clickEventName) {
+      logEvent(clickEventName);
+    }
+
+    openInWebViewer(link);
+  };
+
   return (
     <TouchableOpacity
       accessibilityRole="button"
-      onPress={() => openInWebViewer(link)}
+      onPress={handleOnPress}
       activeOpacity={0.5}
     >
       <View style={S.container}>
