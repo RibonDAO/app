ae70c8ee661b2f3091927042bc83809f7a0f6c07
Nathiely Macedo <nathielymm@gmail.com>
2024-04-16 19:32:49 -0300

    fix: remove unnecessary function

--- src/screens/onboarding/v2/SecondSection/index.tsx
+++ src/screens/onboarding/v2/SecondSection/index.tsx
@@ -1,16 +1,4 @@
-import { useFocusEffect } from "@react-navigation/native";
-import { useFirstAccessToIntegration } from "@ribon.io/shared/hooks";
-import { useIntegrationContext } from "contexts/integrationContext";
-import { useTicketsContext } from "contexts/ticketsContext";
-import { useTickets } from "hooks/useTickets";
-import { setLocalStorageItem } from "lib/localStorage";
-import {
-  RECEIVED_TICKET_AT_KEY,
-  RECEIVED_TICKET_FROM_INTEGRATION,
-} from "lib/localStorage/constants";
-import { useCallback } from "react";
 import { useTranslation } from "react-i18next";
-import { logEvent } from "services/analytics";
 import RibonLogo from "../FirstSection/assets/logo-ribon";
 import Ticket from "./assets/ticket";
 import * as S from "./styles";
@@ -19,39 +7,6 @@ function SecondSection(): JSX.Element {
   const { t } = useTranslation("translation", {
     keyPrefix: "onboarding.v2.secondSection",
   });
-
-  const { hasReceivedTicketToday, handleCanCollect } = useTickets();
-  const { currentIntegrationId, externalId } = useIntegrationContext();
-  const { isFirstAccessToIntegration } =
-    useFirstAccessToIntegration(currentIntegrationId);
-  const { refetchTickets } = useTicketsContext();
-
-  async function receiveTicket() {
-    const canCollect = await handleCanCollect();
-    const receivedTicketToday = await hasReceivedTicketToday();
-    if (canCollect) {
-      if (!receivedTicketToday) {
-        await setLocalStorageItem(
-          RECEIVED_TICKET_AT_KEY,
-          Date.now().toString(),
-        );
-        await setLocalStorageItem(
-          RECEIVED_TICKET_FROM_INTEGRATION,
-          currentIntegrationId?.toLocaleString(),
-        );
-        logEvent("receiveTicket_view", { from: "receivedTickets_toast" });
-      }
-    } else {
-      refetchTickets();
-    }
-  }
-  useFocusEffect(
-    useCallback(() => {
-      if (isFirstAccessToIntegration !== undefined) {
-        receiveTicket();
-      }
-    }, [isFirstAccessToIntegration, externalId]),
-  );
 
   return (
     <S.Container>
