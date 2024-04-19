af5835e7370253e3c9c8a6cf08b1cf0c11ca43f3
Henrique Barbosa <henrikleod@gmail.com>
2024-04-19 18:44:57 -0300

    feat: new ticket animation component

---
+++ src/components/atomics/animations/NewTicketAnimation/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components/native";
+import { theme } from "@ribon.io/shared/styles";
+import { defaultBodyMdBold } from "styles/typography/default";
+
+export const Container = styled.View`
+  position: absolute;
+  padding: 4px 12px;
+  background-color: ${theme.colors.brand.primary[300]};
+  border-radius: 10px;
+  margin-top: -40px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+`;
+
+export const Count = styled.Text`
+  ${defaultBodyMdBold}
+  color: ${theme.colors.brand.primary[700]};
+`;
