ec916674c8cc27346488a1d163a2e9b5182e9599
Henrique Barbosa <henrikleod@gmail.com>
2024-04-29 11:53:12 -0300

    feat: tests

---
+++ src/screens/donations/CausesScreen/CausesSection/CausesList/index.test.tsx
@@ -0,0 +1,48 @@
+import { waitForPromises } from "config/testUtils";
+import {
+  expectTextNotToBeInTheDocument,
+  expectTextToBeInTheDocument,
+} from "config/testUtils/expects";
+import { renderComponent } from "config/testUtils/renders";
+import { causeFactory } from "@ribon.io/shared/config";
+import CausesList from ".";
+
+const mockCause = causeFactory({
+  id: 1,
+  name: "Animal",
+  withPoolBalance: false,
+});
+
+const mockCause2 = causeFactory({
+  id: 2,
+  name: "Health",
+  withPoolBalance: true,
+});
+
+jest.mock("@ribon.io/shared/hooks", () => ({
+  __esModule: true,
+  ...jest.requireActual("@ribon.io/shared/hooks"),
+  useCauses: () => ({
+    causes: [mockCause, mockCause2],
+    refetch: () => {},
+  }),
+}));
+
+describe("CausesList", () => {
+  beforeEach(async () => {
+    renderComponent(<CausesList />);
+    await waitForPromises();
+  });
+
+  it("should render without error", () => {
+    expectTextToBeInTheDocument("All");
+  });
+
+  it("should render the causes with pool balance", () => {
+    expectTextToBeInTheDocument("Health");
+  });
+
+  it("should not render the causes without pool balance", () => {
+    expectTextNotToBeInTheDocument("Animal");
+  });
+});
