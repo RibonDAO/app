81d35fc22c1c7e41e940fa2aa2689aa3f2bf02a1
Henrique Barbosa <henrikleod@gmail.com>
2024-04-29 11:27:35 -0300

    fix: spacing

--- src/screens/donations/CausesScreen/CausesSection/CausesList/styles.ts
+++ src/screens/donations/CausesScreen/CausesSection/CausesList/styles.ts
@@ -1,8 +1,9 @@
+import { theme } from "@ribon.io/shared/styles";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-  padding-left: 16px;
-  padding-right: 16px;
+  padding-left: ${theme.spacingNative(16)}px;
+  padding-right: ${theme.spacingNative(16)}px;
 `;
 
 export const Scroll = styled.ScrollView``;
