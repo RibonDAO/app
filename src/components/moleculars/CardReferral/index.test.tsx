40784ffa6466ebd56bbebe0f158a545a0e3552f0
Henrique Barbosa <henrikleod@gmail.com>
2024-05-07 17:13:17 -0300

    fix: broken tests

--- src/components/moleculars/CardReferral/index.test.tsx
+++ src/components/moleculars/CardReferral/index.test.tsx
@@ -2,6 +2,22 @@ import { renderComponent } from "config/testUtils/renders";
 import { expectTextToBeInTheDocument } from "config/testUtils/expects";
 import CardReferral from ".";
 
+jest.mock("@ribon.io/shared/hooks", () => ({
+  __esModule: true,
+  ...jest.requireActual("@ribon.io/shared/hooks"),
+  useUserProfile: () => ({
+    userProfile: () => ({
+      profile: {
+        photo: "photo",
+        name: "name",
+        user: {
+          email: "email",
+        },
+      },
+    }),
+  }),
+}));
+
 describe("CardLargeImage", () => {
   it("renders without error", () => {
     renderComponent(<CardReferral />);
