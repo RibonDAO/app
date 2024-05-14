7d0a323a880982f99c42e1e34502a5a7e3094047
letbueno <leticia.bueno.martins@gmail.com>
2024-05-14 13:25:50 -0300

    feat: add last card ong

---
+++ src/components/moleculars/LastCard/index.tsx
@@ -0,0 +1,54 @@
+import { NonProfit, theme } from "@ribon.io/shared";
+import Button from "components/atomics/buttons/Button";
+import { useTranslation } from "react-i18next";
+import * as S from "./styles";
+import DefaultBackground from "./assets/DefaultBackground";
+
+type Props = {
+  nonProfit: NonProfit;
+  primaryButtonClick: () => void;
+  secondaryButtonClick: () => void;
+};
+
+function LastCard({
+  nonProfit,
+  primaryButtonClick,
+  secondaryButtonClick,
+}: Props) {
+  const { t } = useTranslation("translation", {
+    keyPrefix: "components.moleculars.lastCard",
+  });
+
+  return (
+    <S.Container>
+      <S.ImageBackground>
+        <DefaultBackground />
+      </S.ImageBackground>
+      <S.OngLogo source={{ uri: nonProfit?.logo }} />
+      <S.OngName>{nonProfit?.name}</S.OngName>
+
+      <S.ButtonContainer>
+        <Button
+          text={t("primaryButtonText")}
+          onPress={primaryButtonClick}
+          backgroundColor={theme.colors.brand.primary[600]}
+          borderColor={theme.colors.brand.primary[600]}
+          textColor={theme.colors.neutral10}
+          customStyles={{ borderRadius: 12 }}
+        />
+        <Button
+          text={t("secondaryButtonText")}
+          onPress={secondaryButtonClick}
+          borderColorOutline={theme.colors.brand.primary[600]}
+          textColorOutline={theme.colors.brand.primary[600]}
+          customStyles={{ borderRadius: 12 }}
+          outline
+        />
+      </S.ButtonContainer>
+
+      <S.OngIcon source={{ uri: nonProfit?.icon }} />
+    </S.Container>
+  );
+}
+
+export default LastCard;
