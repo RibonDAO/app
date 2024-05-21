ef1fd6d0cbd9690896eab0d38f554a9a4061f558
Pedro Santos <pedrosantosdevelop@gmail.com>
2024-05-21 15:39:10 -0300

    test: adds tests for accordion

---
+++ src/components/moleculars/Accordion/index.test.tsx
@@ -0,0 +1,24 @@
+import { screen, render, fireEvent } from "@testing-library/react-native";
+import Accordion from ".";
+
+describe("CardCenterImageButton", () => {
+  it("should render without error", async () => {
+    render(
+      <Accordion
+        title="titleTest"
+        subtitle="subTest"
+        iconUrl="testUrl"
+        description="descTest"
+        isExpansible
+      />,
+    );
+
+    expect(screen.getByText("titleTest")).toBeDefined();
+    expect(screen.getByText("subTest")).toBeDefined();
+    expect(screen.queryByText("descTest")).toBeNull();
+
+    fireEvent(screen.getByText("titleTest"), "pressIn");
+    const description = await screen.findByText("descTest");
+    expect(description).toBeDefined();
+  });
+});
