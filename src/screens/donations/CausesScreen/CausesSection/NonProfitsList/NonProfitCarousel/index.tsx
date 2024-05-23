7b81d58905b691ab693de89bff1f3eac6ccbfefc
Nathiely Macedo <nathielymm@gmail.com>
2024-05-23 13:57:31 -0300

    fix: currency

--- src/screens/donations/CausesScreen/CausesSection/NonProfitsList/NonProfitCarousel/index.tsx
+++ src/screens/donations/CausesScreen/CausesSection/NonProfitsList/NonProfitCarousel/index.tsx
@@ -1,11 +1,12 @@
 import React, { useCallback } from "react";
-import { NonProfit, Story } from "@ribon.io/shared/types";
+import { Currencies, NonProfit, Story } from "@ribon.io/shared/types";
 import { logEvent } from "services/analytics";
 import { useNavigation } from "hooks/useNavigation";
 import { useCurrentUser } from "contexts/currentUserContext";
 import { useTicketsContext } from "contexts/ticketsContext";
 import { FlatList } from "react-native";
 import { useTranslation } from "react-i18next";
+import { useLanguage } from "contexts/languageContext";
 import CardNonProfit from "./components/CardNonProfit";
 import CardNonProfitStories from "./components/CardNonProfitStories";
 import * as S from "./styles";
@@ -22,6 +23,10 @@ function NonProfitCarousel({ nonProfit }: Props) {
   const { navigateTo } = useNavigation();
   const { hasTickets, ticketsCounter } = useTicketsContext();
   const { signedIn } = useCurrentUser();
+  const { currentLang } = useLanguage();
+
+  const currentCurrency =
+    currentLang === "pt-BR" ? Currencies.BRL : Currencies.USD;
 
   const minNumberOfTickets =
     nonProfit?.nonProfitImpacts?.[0]?.minimumNumberOfTickets ?? 0;
@@ -43,76 +48,66 @@ function NonProfitCarousel({ nonProfit }: Props) {
   }, [signedIn, navigateTo, nonProfit]);
 
   const handleDirectDonationButtonPress = useCallback(() => {
-    logEvent("donateTicketBtn_start", {
+    logEvent("giveNgoBtn_start", {
       nonProfitId: nonProfit.id,
       from: "nonprofitCard",
     });
-    if (signedIn) {
-      navigateTo("CheckoutScreen", {
-        nonProfit,
-        cause: nonProfit.cause,
-        target: "non_profit",
-        offer: 0,
-        targetId: nonProfit.id,
-      });
-    } else {
-      navigateTo("DonationSignInScreen", { nonProfit });
-    }
-  }, [signedIn, navigateTo, nonProfit]);
+    navigateTo("CheckoutScreen", {
+      nonProfit,
+      cause: nonProfit.cause,
+      target: "non_profit",
+      targetId: nonProfit.id,
+      offer: 0,
+      currency: currentCurrency,
+    });
+  }, [navigateTo, nonProfit]);
 
   type RenderItemProps = {
     item: Story | null;
     index: number;
   };
 
-  const renderItem = useCallback(
-    ({ item, index }: RenderItemProps) => {
-      if (index === 0) {
-        return (
-          <S.NonProfitContainer isFirst>
-            <CardNonProfit
-              nonProfit={nonProfit}
-              ticketsQuantity={minNumberOfTickets}
-              buttonText={t(
-                minNumberOfTickets > 1 ? "buttonTextPlural" : "buttonText",
-              )}
-              onButtonClick={handleDonateTicketButtonPress}
-              buttonDisabled={!hasEnoughTickets}
-            />
-          </S.NonProfitContainer>
-        );
-      } else if (
-        (nonProfit.stories && index === nonProfit.stories.length + 1) ||
-        nonProfit.stories?.length === 0
-      ) {
-        return (
-          <S.NonProfitContainer isLast>
-            <LastCard
-              nonProfit={nonProfit}
-              primaryButtonClick={handleDonateTicketButtonPress}
-              secondaryButtonClick={handleDirectDonationButtonPress}
-            />
-          </S.NonProfitContainer>
-        );
-      } else {
-        return item ? (
-          <S.NonProfitContainer>
-            <CardNonProfitStories
-              markdownText={item?.description}
-              backgroundImage={item?.image}
-            />
-          </S.NonProfitContainer>
-        ) : null;
-      }
-    },
-    [
-      nonProfit,
-      minNumberOfTickets,
-      handleDonateTicketButtonPress,
-      handleDirectDonationButtonPress,
-      hasEnoughTickets,
-    ],
-  );
+  const renderItem = ({ item, index }: RenderItemProps) => {
+    if (index === 0) {
+      return (
+        <S.NonProfitContainer isFirst>
+          <CardNonProfit
+            nonProfit={nonProfit}
+            ticketsQuantity={minNumberOfTickets}
+            buttonText={t(
+              minNumberOfTickets > 1 ? "buttonTextPlural" : "buttonText",
+            )}
+            onButtonClick={handleDonateTicketButtonPress}
+            buttonDisabled={!hasEnoughTickets}
+          />
+        </S.NonProfitContainer>
+      );
+    } else if (
+      (nonProfit.stories && index === nonProfit.stories.length + 1) ||
+      nonProfit.stories?.length === 0
+    ) {
+      return (
+        <S.NonProfitContainer isLast>
+          <LastCard
+            nonProfit={nonProfit}
+            primaryButtonClick={handleDonateTicketButtonPress}
+            secondaryButtonClick={handleDirectDonationButtonPress}
+          />
+        </S.NonProfitContainer>
+      );
+    } else if (item) {
+      return (
+        <S.NonProfitContainer>
+          <CardNonProfitStories
+            markdownText={item.description}
+            backgroundImage={item.image}
+          />
+        </S.NonProfitContainer>
+      );
+    } else {
+      return null;
+    }
+  };
 
   const data = nonProfit.stories?.length
     ? [null, ...nonProfit.stories, null]
