0f696dca1633c79fe74d3a5aae56c5e42f60a225
Nathiely Macedo <nathielymm@gmail.com>
2024-06-11 13:48:27 -0300

    fix: icon size

--- src/components/moleculars/ImageWithIconOverlay/styles.ts
+++ src/components/moleculars/ImageWithIconOverlay/styles.ts
@@ -13,19 +13,19 @@ export const LeftContainer = styled.View`
   align-items: center;
 `;
 export const LeftImage = styled.Image`
-  width: 96px;
-  height: 96px;
+  width: 104px;
+  height: 104px;
   border: 4px solid ${theme.colors.neutral10};
   border-radius: 48px;
 `;
 export const RightContainer = styled.View`
   width: 128px;
   height: 128px;
-  margin-left: -55px;
+  margin-left: -40px;
   justify-content: center;
   align-items: center;
 `;
 export const RightImage = styled.Image`
-  width: 104px;
-  height: 104px;
+  width: 128px;
+  height: 128px;
 `;
