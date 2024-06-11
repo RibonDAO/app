de07edc094b42cdf6e1357a3bf664ba067e188ba
Nathiely Macedo <nathielymm@gmail.com>
2024-06-11 14:46:42 -0300

    fix: adds user photo fallback

--- src/components/moleculars/ImageWithIconOverlay/index.tsx
+++ src/components/moleculars/ImageWithIconOverlay/index.tsx
@@ -1,14 +1,21 @@
+import UserAvatarIcon from "screens/auth/assets/UserAvatarIcon";
 import * as S from "./styles";
 
 type Props = {
-  leftImage?: string;
+  leftImage?: any;
   rightImage?: string;
 };
 function ImageWithIconOverlay({ leftImage, rightImage }: Props): JSX.Element {
   return (
     <S.Container>
       <S.LeftContainer>
-        <S.LeftImage source={{ uri: leftImage }} testID="leftImage" />
+        <S.LeftImageContainer>
+          {leftImage ? (
+            <S.LeftImage source={{ uri: leftImage }} testID="leftImage" />
+          ) : (
+            <UserAvatarIcon width={104} height={104} />
+          )}
+        </S.LeftImageContainer>
       </S.LeftContainer>
       <S.RightContainer>
         <S.RightImage source={{ uri: rightImage }} testID="rightImage" />
