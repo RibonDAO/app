f825cefc0c73fc5bc457cc5d50acb25e36eacecf
letbueno <leticia.bueno.martins@gmail.com>
2024-06-12 15:17:05 -0300

    fix: remove toast on causes screen

--- src/screens/donations/CausesScreen/Header/index.tsx
+++ src/screens/donations/CausesScreen/Header/index.tsx
@@ -7,36 +7,9 @@ import { useNavigation } from "hooks/useNavigation";
 import { useTicketsContext } from "contexts/ticketsContext";
 import { logEvent } from "services/analytics";
 import HeaderTemplate from "components/moleculars/HeaderTemplate";
-
-import React, { useCallback, useEffect, useState } from "react";
-import NewTicketAnimation from "components/atomics/animations/NewTicketAnimation";
-import { perform } from "lib/timeoutHelpers";
-import { View } from "react-native";
 import NotificationPaymentFailed from "../NotificationPaymentFailed";
 import RibonLogo from "./assets/RibonLogo";
 import * as S from "./styles";
-
-const MemoizedTicketCounter = React.memo(
-  ({ ticketCount }: { ticketCount: number }) => (
-    <S.TicketCountWrapper key={ticketCount}>
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          gap: 8,
-        }}
-      >
-        <S.TicketCounterText>{ticketCount}</S.TicketCounterText>
-        <Icon
-          type="outlined"
-          name="confirmation_number"
-          size={24}
-          color={theme.colors.neutral10}
-        />
-      </View>
-    </S.TicketCountWrapper>
-  ),
-);
 
 function Header(): JSX.Element {
   const { t } = useTranslation("translation", {
@@ -45,9 +18,6 @@ function Header(): JSX.Element {
 
   const { navigateTo, popNavigation } = useNavigation();
   const { ticketsCounter: tickets } = useTicketsContext();
-  const [ticketCount, setTicketCount] = useState(tickets);
-  const [showToast, setShowToast] = useState(false);
-  const [ticketDiff, setTicketDiff] = useState(0);
 
   const handleToggleTooltip = () => {
     logEvent("earnTicketsCTA_click");
@@ -64,35 +34,6 @@ function Header(): JSX.Element {
     logEvent("aboutTicketTooltip_click");
     navigateTo("EarnTicketsScreen");
   };
-
-  const renderTicketCounter = useCallback(
-    () => <MemoizedTicketCounter ticketCount={ticketCount} />,
-    [ticketCount],
-  );
-
-  useEffect(() => {
-    if (tickets !== ticketCount) {
-      if (tickets > ticketCount) {
-        setTicketDiff(tickets - ticketCount);
-      }
-      setTicketCount(tickets);
-    }
-  }, [tickets]);
-
-  useEffect(() => {
-    if (ticketDiff <= 0) return;
-
-    perform(() => setShowToast(true)).in(500);
-  }, [ticketDiff]);
-
-  useEffect(() => {
-    const resetToast = () => {
-      setTicketDiff(0);
-      setShowToast(false);
-    };
-
-    if (showToast) perform(resetToast).in(2000);
-  }, [showToast]);
 
   return (
     <HeaderTemplate>
@@ -119,8 +60,15 @@ function Header(): JSX.Element {
             />
           </S.IconContainer>
         </S.TextContainer>
-        <S.TicketCounter>{renderTicketCounter()}</S.TicketCounter>
-        {showToast && <NewTicketAnimation count={ticketDiff} />}
+        <S.TicketCounter>
+          <S.TicketCounterText>{tickets}</S.TicketCounterText>
+          <Icon
+            type="outlined"
+            name="confirmation_number"
+            size={24}
+            color={theme.colors.neutral10}
+          />
+        </S.TicketCounter>
         <S.TicketLink onPress={handleToggleEarnTickets}>
           {t("earnMoreTickets")}
         </S.TicketLink>
