b831c9a78708a44c74d2fe2551dcaa16bd58fb0e
letbueno <leticia.bueno.martins@gmail.com>
2024-06-17 17:21:16 -0300

    fix: add sort non profits

--- src/screens/donations/CausesScreen/CausesSection/index.tsx
+++ src/screens/donations/CausesScreen/CausesSection/index.tsx
@@ -1,6 +1,7 @@
 import { useNonProfitsContext } from "contexts/nonProfitsContext";
 
 import { useTagDonationContext } from "contexts/tagDonationContext";
+import { useCausesContext } from "contexts/causesContext";
 import CausesFilter from "./TagsFilter";
 import NonProfitsList from "./NonProfitsList";
 import * as S from "./styles";
@@ -16,10 +17,25 @@ export default function CausesSection() {
     return nonProfits || [];
   };
 
+  const sortNonProfits = () => {
+    const filteredNonProfits = filterNonProfits();
+    const { causesWithPoolBalance: causes } = useCausesContext();
+
+    return filteredNonProfits.slice().sort((a, b) => {
+      const causeAId = a.cause.id;
+      const causeBId = b.cause.id;
+
+      return (
+        causes.findIndex((cause) => cause.id === causeAId) -
+        causes.findIndex((cause) => cause.id === causeBId)
+      );
+    });
+  };
+
   return (
     <S.Container>
       <CausesFilter />
-      <NonProfitsList nonProfits={filterNonProfits()} />
+      <NonProfitsList nonProfits={sortNonProfits()} />
     </S.Container>
   );
 }
