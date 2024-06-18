2c27a650ac2ec17f758176219fb9d99c22aac137
Pedro Santos <pedrosantosdevelop@gmail.com>
2024-06-18 18:10:31 -0300

    fix: empty component translation

--- src/components/moleculars/AccordionList/EmptyComponent.tsx
+++ src/components/moleculars/AccordionList/EmptyComponent.tsx
@@ -2,24 +2,24 @@ import { View } from "react-native";
 import { useNavigation } from "hooks/useNavigation";
 import HoldingHeartIcon from "components/vectors/HoldingHeartIcon";
 import Button from "components/atomics/buttons/Button";
+import { useTranslation } from "react-i18next";
 import * as S from "./styles";
 
 export function EmptyComponent() {
+  const { t } = useTranslation("translation", {
+    keyPrefix: "users.impactScreen.profileSection.emptyComponent",
+  });
   const { navigateTo } = useNavigation();
 
   return (
     <S.EmptyComponentContainer>
       <HoldingHeartIcon />
       <View>
-        <S.EmptyComponentText>
-          Suas doações aparecerão aqui.
-        </S.EmptyComponentText>
-        <S.EmptyComponentText>
-          Doe seu primeiro ticket e comece a construir seu impacto!
-        </S.EmptyComponentText>
+        <S.EmptyComponentText>{t("firstParagraph")}</S.EmptyComponentText>
+        <S.EmptyComponentText>{t("secondParagraph")}</S.EmptyComponentText>
       </View>
       <Button
-        text="Doar tickets"
+        text={t("button")}
         onPress={() => navigateTo("TabNavigator", { screen: "CausesScreen" })}
       />
     </S.EmptyComponentContainer>
