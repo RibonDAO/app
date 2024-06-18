5aef317c821a91812da95dfc47c2769b8d5d6c65
letbueno <leticia.bueno.martins@gmail.com>
2024-06-18 09:48:47 -0300

    fix: sort non profits randomically

--- src/screens/donations/CausesScreen/CausesSection/index.tsx
+++ src/screens/donations/CausesScreen/CausesSection/index.tsx
@@ -1,7 +1,6 @@
 import { useNonProfitsContext } from "contexts/nonProfitsContext";
 
 import { useTagDonationContext } from "contexts/tagDonationContext";
-import { useCausesContext } from "contexts/causesContext";
 import CausesFilter from "./TagsFilter";
 import NonProfitsList from "./NonProfitsList";
 import * as S from "./styles";
@@ -17,25 +16,12 @@ export default function CausesSection() {
     return nonProfits || [];
   };
 
-  const sortNonProfits = () => {
-    const filteredNonProfits = filterNonProfits();
-    const { causesWithPoolBalance: causes } = useCausesContext();
-
-    return filteredNonProfits.slice().sort((a, b) => {
-      const causeAId = a.cause.id;
-      const causeBId = b.cause.id;
-
-      return (
-        causes.findIndex((cause) => cause.id === causeAId) -
-        causes.findIndex((cause) => cause.id === causeBId)
-      );
-    });
-  };
+  const shuffledNonProfits = filterNonProfits().sort(() => 0.5 - Math.random());
 
   return (
     <S.Container>
       <CausesFilter />
-      <NonProfitsList nonProfits={sortNonProfits()} />
+      <NonProfitsList nonProfits={shuffledNonProfits} />
     </S.Container>
   );
 }
