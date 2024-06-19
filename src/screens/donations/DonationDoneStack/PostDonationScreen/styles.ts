a45aa8dfa26d65fb0680a3cf5473a9e5f1c84598
Nathiely Macedo <nathielymm@gmail.com>
2024-06-19 15:30:56 -0300

    feat: adds post donation screen

--- src/screens/donations/DonationDoneStack/PostDonationScreen/styles.ts
+++ src/screens/donations/DonationDoneStack/PostDonationScreen/styles.ts
@@ -1,53 +1,41 @@
+import styled from "styled-components/native";
+import { stylizedDisplaySm } from "styles/typography/stylized";
+import { defaultBodyMdMedium } from "styles/typography/default";
 import { theme } from "@ribon.io/shared/styles";
-import { StyleSheet } from "react-native";
-import { stylizedDisplayMd } from "styles/typography/stylized";
 
-const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    justifyContent: "flex-start",
-    alignItems: "center",
-    width: "100%",
-    height: "100%",
-    resizeMode: "cover",
-    marginVertical: theme.spacingNative(20),
-    padding: theme.spacingNative(16),
-  },
-  text: {
-    ...stylizedDisplayMd,
-    marginBottom: theme.spacingNative(24),
-    fontSize: 26,
-    fontWeight: "bold",
-    lineHeight: 36,
-  },
-  imageContainer: {
-    width: 96,
-    height: 96,
-    shadowColor: "rgba(40, 36, 28, 0.2)",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowRadius: 10,
-    shadowOpacity: 0.8,
-    marginBottom: theme.spacingNative(24),
-  },
-  imageBackground: {
-    width: "100%",
-    height: "100%",
-    borderRadius: 16,
-  },
-  buttonContainer: {
-    width: "100%",
-    position: "absolute",
-    bottom: 24,
-    paddingBottom: theme.spacingNative(16),
-  },
-  button: {
-    borderColor: theme.colors.brand.primary[600],
-    borderRadius: 4,
-    height: 48,
-  },
-});
+export const TopContainer = styled.View`
+  width: 100%;
+  align-items: center;
+`;
+export const Container = styled.View`
+  color: ${theme.colors.brand.primary[300]};
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 16px 16px 16px;
+`;
 
-export default styles;
+export const ContentContainer = styled.View`
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  ${stylizedDisplaySm}
+  color: ${theme.colors.neutral[800]};
+`;
+
+export const Description = styled.Text`
+  ${defaultBodyMdMedium}
+  color: ${theme.colors.neutral[600]};
+  text-align: center;
+`;
+export const BackgroundSun = styled.View`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  justify-content: flex-end;
+  align-items: center;
+  z-index: -1;
+`;
