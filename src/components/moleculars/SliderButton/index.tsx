24882df23c592828cf63466e47482f81bfe58962
Thais Correa <thais@ribon.io>
2024-07-03 18:08:20 -0300

    fix: lottie 3 steps

--- src/components/moleculars/SliderButton/index.tsx
+++ src/components/moleculars/SliderButton/index.tsx
@@ -30,7 +30,7 @@ function SliderButton({
     setValue(newValue);
   };
 
-  const minimumValue = rangeSize < 2 * step ? 0 : step;
+  const minimumValue = step;
   const maximumValue = Math.floor(rangeSize / step) * step;
 
   const minusDisabled = sliderValue <= step;
