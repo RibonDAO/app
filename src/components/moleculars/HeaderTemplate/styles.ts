76ac6bef82a26366852b6e0600c8cde3ffef0f3d
Nathiely Macedo <nathielymm@gmail.com>
2024-07-03 12:02:10 -0300

    fix: padding

--- src/components/moleculars/HeaderTemplate/styles.ts
+++ src/components/moleculars/HeaderTemplate/styles.ts
@@ -5,7 +5,7 @@ export const Container = styled.View`
   display: flex;
   padding-bottom: ${theme.spacing(48)};
   width: 100%;
-  padding: ${theme.spacing(0, 16)};
+  padding-horizontal: ${theme.spacing(16)};
   background-color: ${theme.colors.brand.primary[800]};
   display: flex;
   align-items: flex-start;
