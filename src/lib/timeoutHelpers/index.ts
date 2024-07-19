38e89ef66f7fd34f528ab4f85ce182098e39a239
Henrique Barbosa <henrikleod@gmail.com>
2024-07-19 16:49:41 -0300

    feat: countdown timer

--- src/lib/timeoutHelpers/index.ts
+++ src/lib/timeoutHelpers/index.ts
@@ -5,3 +5,26 @@ export function perform(fn: () => void): { in: (delay: number) => void } {
     },
   };
 }
+
+type CountdownProps = {
+  timeInSeconds: number;
+  onChange: (value: number) => void;
+  onComplete: () => void;
+};
+
+export function countdown({
+  timeInSeconds,
+  onChange,
+  onComplete,
+}: CountdownProps) {
+  let seconds = timeInSeconds;
+  const interval = setInterval(() => {
+    seconds -= 1;
+    onChange(seconds);
+
+    if (seconds === 0) {
+      clearInterval(interval);
+      onComplete();
+    }
+  }, 1000);
+}
