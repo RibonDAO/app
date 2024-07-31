524419fc07e40fa38c2d9cb4f7aafcf1672e9de4
Pedro Santos <pedrosantosdevelop@gmail.com>
2024-07-31 10:22:50 -0300

    feat: random post donation color

--- src/screens/donations/DonationDoneStack/PostDonationScreen/index.tsx
+++ src/screens/donations/DonationDoneStack/PostDonationScreen/index.tsx
@@ -8,9 +8,117 @@ import { useAuthentication } from "contexts/authenticationContext";
 import LottieAnimation from "components/atomics/LottieAnimation";
 import { getLocalStorageItem, setLocalStorageItem } from "lib/localStorage";
 import { DONATION_COUNT } from "lib/localStorage/constants";
+import { useState } from "react";
 import postDonationAnimation from "./assets/postDonationAnimation.json";
 import sunAnimation from "./assets/sunAnimation.json";
 import * as S from "./styles";
+
+function generateFaceColorFilter(
+  eyebrownsColor: string,
+  faceColor: string,
+  eyesAndMouthColor: string,
+) {
+  return [
+    {
+      keypath: "olhos",
+      color: eyesAndMouthColor,
+    },
+    {
+      keypath: "boca",
+      color: eyesAndMouthColor,
+    },
+    {
+      keypath: "sobran1",
+      color: eyebrownsColor,
+    },
+    {
+      keypath: "sobran2",
+      color: eyebrownsColor,
+    },
+    {
+      keypath: "rosto1",
+      color: faceColor,
+    },
+    {
+      keypath: "rosto2",
+      color: faceColor,
+    },
+    {
+      keypath: "rosto3",
+      color: faceColor,
+    },
+    {
+      keypath: "rosto4",
+      color: faceColor,
+    },
+    {
+      keypath: "rosto5",
+      color: faceColor,
+    },
+    {
+      keypath: "rosto6",
+      color: faceColor,
+    },
+  ];
+}
+
+function generateSunColorFilter(sunColor: string) {
+  return [
+    {
+      keypath: "sparkVector Outlines",
+      color: sunColor,
+    },
+  ];
+}
+
+function generateRandomColorArgs() {
+  const options = [
+    {
+      eyesAndMouth: theme.colors.brand.primary[500],
+      face: theme.colors.brand.primary[50],
+      eyeBrowns: theme.colors.brand.primary[300],
+      sun: theme.colors.brand.primary[25],
+      button: theme.colors.brand.primary[600],
+    }, // green
+    {
+      eyesAndMouth: theme.colors.brand.quaternary[500],
+      face: theme.colors.brand.quaternary[300],
+      eyeBrowns: theme.colors.brand.quaternary[300],
+      sun: theme.colors.brand.quaternary[25],
+      button: theme.colors.brand.quaternary[600],
+    }, // yellow
+    {
+      eyesAndMouth: theme.colors.brand.tertiary[500],
+      face: theme.colors.brand.tertiary[300],
+      eyeBrowns: theme.colors.brand.tertiary[300],
+      sun: theme.colors.brand.tertiary[25],
+      button: theme.colors.brand.tertiary[600],
+    }, // pink
+    {
+      eyesAndMouth: theme.colors.brand.secondary[500],
+      face: theme.colors.brand.secondary[100],
+      eyeBrowns: theme.colors.brand.secondary[300],
+      sun: theme.colors.brand.secondary[25],
+      button: theme.colors.brand.secondary[600],
+    }, // orange
+    {
+      eyesAndMouth: "#5396C9",
+      face: "#7BC6FF",
+      eyeBrowns: "#7BC6FF",
+      sun: "#F6FBFE",
+      button: "#3E7AA8",
+    },
+  ];
+
+  const randomIndex = Math.floor(Math.random() * options.length);
+  const args = options[randomIndex];
+
+  return {
+    button: args.button,
+    sun: generateSunColorFilter(args.sun),
+    face: generateFaceColorFilter(args.eyeBrowns, args.face, args.eyesAndMouth),
+  };
+}
 
 function PostDonationScreen() {
   usePageView("P39_view");
@@ -21,6 +129,7 @@ function PostDonationScreen() {
   const { isAuthenticated } = useAuthentication();
   const { navigateTo } = useNavigation();
   const { warmGlowMessage, isLoading } = useWarmGlowMessages();
+  const [colorArgs] = useState(generateRandomColorArgs());
 
   const shouldAskForReview = async () => {
     const donations = await getLocalStorageItem(DONATION_COUNT);
@@ -59,6 +168,7 @@ function PostDonationScreen() {
             animationData={postDonationAnimation}
             width={428}
             height={428}
+            colorFilters={colorArgs.face}
           />
         </S.TopContainer>
         <S.ContentContainer>
@@ -73,8 +183,8 @@ function PostDonationScreen() {
             color: theme.colors.neutral10,
           }}
           customStyles={{
-            backgroundColor: theme.colors.brand.primary[600],
-            borderColor: theme.colors.brand.primary[800],
+            backgroundColor: colorArgs.button,
+            borderColor: colorArgs.button,
             borderRadius: 12,
           }}
         />
@@ -84,6 +194,7 @@ function PostDonationScreen() {
           animationData={sunAnimation}
           width="100%"
           height={262}
+          colorFilters={colorArgs.sun}
         />
       </S.BackgroundSun>
     </>
