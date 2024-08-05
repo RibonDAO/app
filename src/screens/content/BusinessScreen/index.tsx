2fecda29a8fc271c23dba6b8165d6108e715c6c5
Nathiely Macedo <nathielymm@gmail.com>
2024-08-05 09:54:10 -0300

    feat: adds animation and business info

--- src/screens/content/BusinessScreen/index.tsx
+++ src/screens/content/BusinessScreen/index.tsx
@@ -1,15 +1,15 @@
-import { useSubscriptions } from "@ribon.io/shared/hooks";
-import { useEffect } from "react";
 import { useCurrentUser } from "contexts/currentUserContext";
 import { useNavigation } from "hooks/useNavigation";
 import Button from "components/atomics/buttons/Button";
 import { theme } from "@ribon.io/shared/styles";
 import { useTranslation } from "react-i18next";
-import BlueSun from "./assets/BlueSun";
+import { useBusinessSubscriptionContext } from "contexts/businessSubscriptionContext";
+import LottieAnimation from "components/atomics/LottieAnimation";
 import * as S from "./styles";
 import Box from "./assets/Box";
 import Wallet from "./assets/Wallet";
 import BlueTicket from "./assets/BlueTicket";
+import BlueSunAnimation from "./assets/BlueSunAnimation.json";
 import Clock from "./assets/Clock";
 import Bag from "./assets/Bag";
 
@@ -18,77 +18,91 @@ export default function BusinessScreen(): JSX.Element {
     keyPrefix: "content.businessScreen",
   });
 
-  const { userSubscriptions } = useSubscriptions();
-  const { subscriptions } = userSubscriptions();
   const { navigateTo } = useNavigation();
   const { currentUser } = useCurrentUser();
-
-  useEffect(() => {
-    console.log(subscriptions);
-    console.log(currentUser);
-  }, [currentUser, subscriptions]);
+  const { businessPlan } = useBusinessSubscriptionContext();
 
   return (
-    <S.Container>
+    <>
       <S.BackgroundSun>
-        <BlueSun />
+        <LottieAnimation
+          animationData={BlueSunAnimation}
+          width={466}
+          height={466}
+        />
       </S.BackgroundSun>
-      <S.BackgroundBag>
-        <Bag />
-      </S.BackgroundBag>
-      <S.MainContainer>
-        <S.Header>
-          <S.CompanyLogoContainer />
+      <S.Container>
+        <S.BackgroundBag>
+          <Bag />
+        </S.BackgroundBag>
+        <S.MainContainer>
+          <S.Header>
+            <S.CompanyLogoContainer>
+              <S.CompanyLogo source={{ uri: currentUser?.company?.logo }} />
+            </S.CompanyLogoContainer>
 
-          <S.TextContainer>
-            <S.Title>{t("title")}</S.Title>
+            <S.TextContainer>
+              <S.Title>{t("title")}</S.Title>
 
-            <S.Subtitle>{t("subtitle")}</S.Subtitle>
-          </S.TextContainer>
-        </S.Header>
-        <S.BenefitsContainer>
-          <S.Benefit>
-            <BlueTicket />
-            <S.BenefitTextContainer>
-              <S.BenefitTitle>{t("firstBenefitTitle")}</S.BenefitTitle>
-              <S.BenefitText>{t("firstBenefitDescription")}</S.BenefitText>
-            </S.BenefitTextContainer>
-          </S.Benefit>
-          <S.Benefit>
-            <Box />
-            <S.BenefitTextContainer>
-              <S.BenefitTitle>{t("secondBenefitTitle")}</S.BenefitTitle>
-              <S.BenefitText>{t("secondBenefitDescription")}</S.BenefitText>
-            </S.BenefitTextContainer>
-          </S.Benefit>
-          <S.Benefit>
-            <Wallet />
+              <S.Subtitle>
+                {t("subtitle", {
+                  companyName: currentUser?.company?.name,
+                })}
+              </S.Subtitle>
+            </S.TextContainer>
+          </S.Header>
+          <S.BenefitsContainer>
+            <S.Benefit>
+              <BlueTicket />
+              <S.BenefitTextContainer>
+                <S.BenefitTitle>{t("firstBenefitTitle")}</S.BenefitTitle>
+                <S.BenefitText>
+                  {t("firstBenefitDescription", {
+                    dailyTickets: businessPlan?.dailyTickets,
+                  })}
+                </S.BenefitText>
+              </S.BenefitTextContainer>
+            </S.Benefit>
+            <S.Benefit>
+              <Box />
+              <S.BenefitTextContainer>
+                <S.BenefitTitle>{t("secondBenefitTitle")}</S.BenefitTitle>
+                <S.BenefitText>
+                  {t("secondBenefitDescription", {
+                    monthlyTickets: businessPlan?.monthlyTickets,
+                  })}
+                </S.BenefitText>
+              </S.BenefitTextContainer>
+            </S.Benefit>
+            <S.Benefit>
+              <Wallet />
 
-            <S.BenefitTextContainer>
-              <S.BenefitTitle>{t("thirdBenefitTitle")}</S.BenefitTitle>
-              <S.BenefitText>{t("thirdBenefitDescription")}</S.BenefitText>
-            </S.BenefitTextContainer>
-          </S.Benefit>
-          <S.Benefit>
-            <Clock />
-            <S.BenefitTextContainer>
-              <S.BenefitTitle>{t("fourthBenefitTitle")}</S.BenefitTitle>
-              <S.BenefitText>{t("fourthBenefitDescription")}</S.BenefitText>
-            </S.BenefitTextContainer>
-          </S.Benefit>
-        </S.BenefitsContainer>
-        <S.ButtonContainer>
-          <Button
-            text="Continue"
-            onPress={() => {
-              navigateTo("ClubScreen");
-            }}
-            backgroundColor={theme.colors.brand.quinary[800]}
-            borderColor={theme.colors.brand.quinary[800]}
-            textColor={theme.colors.neutral10}
-          />
-        </S.ButtonContainer>
-      </S.MainContainer>
-    </S.Container>
+              <S.BenefitTextContainer>
+                <S.BenefitTitle>{t("thirdBenefitTitle")}</S.BenefitTitle>
+                <S.BenefitText>{t("thirdBenefitDescription")}</S.BenefitText>
+              </S.BenefitTextContainer>
+            </S.Benefit>
+            <S.Benefit>
+              <Clock />
+              <S.BenefitTextContainer>
+                <S.BenefitTitle>{t("fourthBenefitTitle")}</S.BenefitTitle>
+                <S.BenefitText>{t("fourthBenefitDescription")}</S.BenefitText>
+              </S.BenefitTextContainer>
+            </S.Benefit>
+          </S.BenefitsContainer>
+          <S.ButtonContainer>
+            <Button
+              text="Continue"
+              onPress={() => {
+                navigateTo("CausesScreen");
+              }}
+              backgroundColor={theme.colors.brand.quinary[800]}
+              borderColor={theme.colors.brand.quinary[800]}
+              textColor={theme.colors.neutral10}
+            />
+          </S.ButtonContainer>
+        </S.MainContainer>
+      </S.Container>
+    </>
   );
 }
