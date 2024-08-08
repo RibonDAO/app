67bfe3f1eb736b80ec98143ba09e135d3b4941cf
Henrique Barbosa <henrikleod@gmail.com>
2024-08-08 17:55:21 -0300

    refactor: avoid dry

--- src/screens/donations/SelectTicketsScreen/Lottie3Steps/index.tsx
+++ src/screens/donations/SelectTicketsScreen/Lottie3Steps/index.tsx
@@ -13,93 +13,58 @@ export type Props = {
 
 const DURATION = 300;
 
+const enteringKeyframes = new Keyframe({
+  0: {
+    opacity: 0,
+    transform: [{ scale: 0 }],
+  },
+  100: {
+    opacity: 1,
+    transform: [{ scale: 1 }],
+  },
+});
+
+const exitingKeyframes = new Keyframe({
+  0: {
+    opacity: 1,
+    transform: [{ scale: 1 }],
+  },
+  100: {
+    opacity: 0,
+    transform: [{ scale: 0 }],
+  },
+});
+
 export default function Lottie3Steps({
   rangeSize,
   value,
   step,
 }: Props): JSX.Element {
-  const isRangeLocked = step * 2 > rangeSize;
+  // Range is not able to move. Ex: step = 4, rangeSize = 5
+  const locked = step * 2 > rangeSize;
 
-  const rangePercentage = isRangeLocked
-    ? 70
-    : ((value - step) / (rangeSize - step)) * 100;
+  const percent = locked ? 70 : ((value - step) / (rangeSize - step)) * 100;
 
-  const layer1Visible = rangePercentage >= 5;
-  const layer2Visible = rangePercentage >= 50;
-  const layer3Visible = rangePercentage >= 95;
-
-  const enteringKeyframes = new Keyframe({
-    0: {
-      opacity: 0,
-      transform: [{ scale: 0 }],
-    },
-    100: {
-      opacity: 1,
-      transform: [{ scale: 1 }],
-    },
-  });
-
-  const exitingKeyframes = new Keyframe({
-    0: {
-      opacity: 1,
-      transform: [{ scale: 1 }],
-    },
-    100: {
-      opacity: 0,
-      transform: [{ scale: 0 }],
-    },
-  });
+  const renderLottie = (source: any) => (
+    <Animated.View
+      entering={enteringKeyframes.duration(DURATION)}
+      exiting={exitingKeyframes.duration(DURATION)}
+      style={{ position: "absolute", top: 0, left: 0 }}
+    >
+      <Lottie
+        source={source}
+        style={{ width: 360, height: 360 }}
+        loop={false}
+        autoPlay
+      />
+    </Animated.View>
+  );
 
   return (
     <S.Container>
-      {layer1Visible && (
-        <Animated.View
-          entering={enteringKeyframes.duration(DURATION)}
-          exiting={exitingKeyframes.duration(DURATION)}
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-          }}
-        >
-          <Lottie
-            source={step1}
-            style={{ width: 360, height: 360 }}
-            loop={false}
-            autoPlay
-          />
-        </Animated.View>
-      )}
-
-      {layer2Visible && (
-        <Animated.View
-          entering={enteringKeyframes.duration(DURATION)}
-          exiting={exitingKeyframes.duration(DURATION)}
-          style={{ position: "absolute", top: 0, left: 0 }}
-        >
-          <Lottie
-            source={step2}
-            style={{ width: 360, height: 360 }}
-            loop={false}
-            autoPlay
-          />
-        </Animated.View>
-      )}
-
-      {layer3Visible && (
-        <Animated.View
-          entering={enteringKeyframes.duration(DURATION)}
-          exiting={exitingKeyframes.duration(DURATION)}
-          style={{ position: "absolute", top: 0, left: 0 }}
-        >
-          <Lottie
-            source={step3}
-            style={{ width: 360, height: 360 }}
-            loop={false}
-            autoPlay
-          />
-        </Animated.View>
-      )}
+      {percent >= 5 && renderLottie(step1)}
+      {percent >= 55 && renderLottie(step2)}
+      {percent >= 95 && renderLottie(step3)}
     </S.Container>
   );
 }
